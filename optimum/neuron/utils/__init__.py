32b8960e6c0ffafb1f01f8f3b106ecbe9dc028f0
Ubuntu <ubuntu@ip-172-31-33-90.ec2.internal>
2023-02-21 17:31:46 +0000

    Add init

--- optimum/neuron/utils/__init__.py
+++ optimum/neuron/utils/__init__.py
@@ -1,6 +1,7 @@
 from packaging import version
 
 from .. import __version__
+from .import_utils import is_neuron_available, is_neuronx_available
 from .training_utils import (
     FirstAndLastDataset,
     Patcher,
