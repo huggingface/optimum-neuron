837e85f7f655345df218e66601871c5946121c40
JingyaHuang <jingya@huggingface.co>
2023-03-15 15:08:39 +0000

    fix setup

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ QUALITY_REQUIRES = [
 EXTRAS_REQUIRE = {
     "tests": TESTS_REQUIRE,
     "quality": QUALITY_REQUIRES,
-    "neuron": ["neuron-cc[tensorflow]", "torch-neuron", "protobuf==3.20.2"],
+    "neuron": ["neuron-cc[tensorflow]", "torch-neuron", "protobuf==3.20.2", "torchvision"],
 }
 
 setup(
