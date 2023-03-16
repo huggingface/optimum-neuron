77b9654f8262f95de8d4d8ac031d7441d11e8506
JingyaHuang <jingya@huggingface.co>
2023-03-16 10:10:23 +0000

    fix import

--- optimum/neuron/__init__.py
+++ optimum/neuron/__init__.py
@@ -24,8 +24,6 @@ _import_structure = {
 }
 
 if TYPE_CHECKING:
-    import os
-
     from .hf_argparser import TrainiumHfArgumentParser
     from .trainers import Seq2SeqTrainiumTrainer, TrainiumTrainer
     from .utils import is_neuron_available, is_neuronx_available, patch_transformers_for_neuron_sdk
@@ -39,6 +37,9 @@ else:
         _import_structure,
         module_spec=__spec__,
     )
+
+import os
+
 
 if not os.environ.get("DISABLE_TRANSFORMERS_PATCHING", False):
     patch_transformers_for_neuron_sdk()
