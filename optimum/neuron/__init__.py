e709945d233b4c4d3129ce032758b985de7e23ad
JingyaHuang <huang_jingya@outlook.com>
2023-03-16 10:26:53 +0000

    fix import

--- optimum/neuron/__init__.py
+++ optimum/neuron/__init__.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from typing import TYPE_CHECKING
 
 from transformers.utils import _LazyModule
@@ -20,14 +21,11 @@ from transformers.utils import _LazyModule
 _import_structure = {
     "hf_argparser": ["TrainiumHfArgumentParser"],
     "trainers": ["TrainiumTrainer", "Seq2SeqTrainiumTrainer"],
-    "utils": ["is_neuron_available", "is_neuronx_available", "patch_transformers_for_neuron_sdk"],
 }
 
 if TYPE_CHECKING:
     from .hf_argparser import TrainiumHfArgumentParser
     from .trainers import Seq2SeqTrainiumTrainer, TrainiumTrainer
-    from .utils import is_neuron_available, is_neuronx_available, patch_transformers_for_neuron_sdk
-
 else:
     import sys
 
@@ -38,7 +36,10 @@ else:
         module_spec=__spec__,
     )
 
+
 import os
+
+from .utils import is_neuron_available, is_neuronx_available, patch_transformers_for_neuron_sdk
 
 
 if not os.environ.get("DISABLE_TRANSFORMERS_PATCHING", False):
