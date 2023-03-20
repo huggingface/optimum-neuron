4b09bd5aa0713b24d5672d9776cd0cd6aff5b6ec
Michael Benayoun <mickbenayoun@gmail.com>
2023-03-20 11:52:43 +0100

    [WIP] CLI

--- tools/auto_fill_neuron_cache.py
+++ tools/auto_fill_neuron_cache.py
@@ -13,6 +13,40 @@
 # See the License for the specific language governing permissions and
 """Tools that fills the neuron cache with common models for the supported tasks."""
 
+import time
+from argparse import ArgumentParser
+from pathlib import Path
+from typing import TYPE_CHECKING, List, Dict
+
+# TODO: find a cleaner solution to this.
+import sys
+path_tests = Path(__file__).parent.parent / "tests"
+sys.path.insert(0, str(path_tests))
+
+from test_examples import (
+        TextClassificationExampleTester,
+        TokenClassificationExampleTester,
+        MultipleChoiceExampleTester,
+        QuestionAnsweringExampleTester,
+        SummarizationExampleTester,
+        TranslationExampleTester,
+        ImageClassificationExampleTester,
+        ExampleTestMeta,
+)
+
+if TYPE_CHECKING:
+    from test_examples import ExampleTesterBase
+
+TESTER_CLASSES = {
+    "sequence-classification": TextClassificationExampleTester,
+    "token-classification": TokenClassificationExampleTester,
+    "multiple-choice": MultipleChoiceExampleTester,
+    "question-answering": QuestionAnsweringExampleTester,
+    "summarization": SummarizationExampleTester,
+    "translation": TranslationExampleTester,
+    "image-classification": ImageClassificationExampleTester,
+}
+
 
 
 ARCHITECTURES_TO_COMMON_PRETRAINED_WEIGHTS = {
@@ -154,3 +188,87 @@ ARCHITECTURES_TO_COMMON_PRETRAINED_WEIGHTS = {
     },
 
 }
+
+
+def get_testers_for_model_type(model_type: str) -> List["ExampleTesterBase"]:
+    testers = []
+    for task, cls in TESTER_CLASSES.items():
+        for attr_name in dir(cls):
+            if attr_name.startswith("test") and model_type in attr_name:
+                testers.append((task, cls(), attr_name))
+    return testers
+
+
+def remove_extra_command_line_argument(command_prefix: str, extra_command_line_arguments: List[str]):
+    argument_idx = None
+    for idx, cmd_line_argument in enumerate(extra_command_line_arguments):
+        if cmd_line_argument.startswith(command_prefix):
+            argument_idx = idx
+    if argument_idx is not None:
+        extra_command_line_arguments.pop(argument_idx)
+
+def run_auto_fill_cache_for_model_name(model_type: str, model_name: str, shape_values_for_task: Dict[str, int], tester: "ExampleTesterBase", method_name: str, neuron_cache: str):
+    
+    batch_size = shape_values_for_task.get("batch_size")
+    sequence_length = shape_values_for_task.get("sequence_length")
+    source_sequence_length = shape_values_for_task.get("source_sequence_length")
+    target_sequence_length = shape_values_for_task.get("target_sequence_length")
+    
+    extra_command_line_arguments = tester.EXTRA_COMMAND_LINE_ARGUMENTS
+    if extra_command_line_arguments is None:
+        extra_command_line_arguments = []
+    
+    if batch_size is not None:
+        tester.TRAIN_BATCH_SIZE = batch_size
+        tester.EVAL_BATCH_SIZE = batch_size
+    
+    if sequence_length is not None:
+        remove_extra_command_line_argument("--max_seq_length", extra_command_line_arguments)
+        extra_command_line_arguments.append(f"--max_seq_length {sequence_length}")
+    
+    if source_sequence_length is not None:
+        remove_extra_command_line_argument("--max_source_length", extra_command_line_arguments)
+        extra_command_line_arguments.append(f"--max_source_length {source_sequence_length}")
+    
+    if target_sequence_length is not None:
+        remove_extra_command_line_argument("--max_target_length", extra_command_line_arguments)
+        extra_command_line_arguments.append(f"--max_target_length {target_sequence_length}")
+
+    tester.EXTRA_COMMAND_LINE_ARGUMENTS = extra_command_line_arguments
+    tester.NEURON_CACHE = neuron_cache
+    tester.ONLY_PRECOMPILATION = True
+    
+    setattr(tester, method_name, ExampleTestMeta._create_test(model_type, model_name))
+    getattr(tester, method_name)()
+
+
+
+def parse_args():
+    parser = ArgumentParser(description="Tool that runs precompilation to fill the Neuron Cache.")
+    parser.add_argument("--cache-path", type=Path, default="neuron-cache", help="The directory in which all the precompiled neff files will be stored.")
+    parser.add_argument("--models", type=str, default="all", nargs="+",help="The models to precompile.")
+    parser.add_argument("--tasks", type=str, default="all", nargs="+",help="The tasks to precompile.")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    if args.models == "all":
+        models = list(ARCHITECTURES_TO_COMMON_PRETRAINED_WEIGHTS.keys())
+    else:
+        models = args.models
+    
+    for model_type in models:
+        testers = get_testers_for_model_type(model_type)
+        for task, tester, method_name in testers:
+            if args.tasks != "all" and task not in args.tasks:
+                continue
+            for model_name, shape_values in ARCHITECTURES_TO_COMMON_PRETRAINED_WEIGHTS[model_type].items():
+                print(f"Running precompilation for {model_name} on {task}...")
+                start = time.time()
+                shape_values_for_task = shape_values.get(task, shape_values["default"])
+                run_auto_fill_cache_for_model_name(model_type, model_name, shape_values_for_task, tester, method_name, args.cache_path)
+                end = time.time()
+                print(f"Done! Duration: {end - start:.3f}.")
+
