7478b4c33df6f4ee2fb6437cf66e36b1624d1441
Michael Benayoun <mickbenayoun@gmail.com>
2023-04-14 10:59:44 +0200

    Make tests inferentia tests

--- tests/exporters/test_cli.py
+++ tests/exporters/test_cli.py
@@ -25,6 +25,7 @@ from parameterized import parameterized
 from optimum.exporters.neuron.model_configs import *  # noqa: F403
 from optimum.exporters.tasks import TasksManager
 from optimum.neuron.utils import is_neuron_available
+from optimum.neuron.utils.testing_utils import is_inferentia_test
 from optimum.utils import DEFAULT_DUMMY_SHAPES, logging
 
 from .exporters_utils import EXPORT_MODELS_TINY
@@ -96,6 +97,7 @@ def _get_commands_to_test(models_to_test):
     return sorted(commands_to_test)
 
 
+@is_inferentia_test
 class TestCLI(unittest.TestCase):
     def test_helps_no_raise(self):
         commands = [
