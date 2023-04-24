5bbcf1629f1fb10d2b2ca15bfb68cd2678566f98
JingyaHuang <huang_jingya@outlook.com>
2023-04-24 16:41:51 +0000

    Add other neuron models for nlp

--- optimum/neuron/modeling.py
+++ optimum/neuron/modeling.py
@@ -20,13 +20,19 @@ import torch
 from transformers import (
     AutoModel,
     AutoModelForMaskedLM,
+    AutoModelForMultipleChoice,
+    AutoModelForQuestionAnswering,
     AutoModelForSequenceClassification,
+    AutoModelForTokenClassification,
 )
 from transformers.file_utils import add_start_docstrings, add_start_docstrings_to_model_forward
 from transformers.modeling_outputs import (
     BaseModelOutput,
     MaskedLMOutput,
+    MultipleChoiceModelOutput,
+    QuestionAnsweringModelOutput,
     SequenceClassifierOutput,
+    TokenClassifierOutput,
 )
 
 from .modeling_base import NeuronModel
@@ -163,7 +169,7 @@ MASKED_LM_EXAMPLE = r"""
     >>> outputs = model(**inputs)
     >>> logits = outputs.logits
     >>> list(logits.shape)
-    [1, 8, 28996]
+    [2, 8, 28996]
     ```
 
     Example using `transformers.pipeline`:
@@ -223,6 +229,84 @@ class NeuronModelForMaskedLM(NeuronModel):
 
         # converts output to namedtuple for pipelines post-processing
         return MaskedLMOutput(logits=logits)
+
+
+QUESTION_ANSWERING_EXAMPLE = r"""
+    Example of question answering:
+
+    ```python
+    >>> from transformers import {processor_class}
+    >>> from optimum.neuron import {model_class}
+
+    >>> tokenizer = {processor_class}.from_pretrained("{checkpoint}")
+    >>> model = {model_class}.from_pretrained("{checkpoint}")
+
+    >>> question, text = "Who was Jim Henson?", "Jim Henson was a nice puppet"
+    >>> inputs = tokenizer(question, text, return_tensors="pt")
+    >>> start_positions = torch.tensor([1])
+    >>> end_positions = torch.tensor([3])
+
+    >>> outputs = model(**inputs, start_positions=start_positions, end_positions=end_positions)
+    >>> start_scores = outputs.start_logits
+    >>> end_scores = outputs.end_logits
+    ```
+    Example using `transformers.pipeline`:
+
+    ```python
+    >>> from transformers import {processor_class}, pipeline
+    >>> from optimum.neuron import {model_class}
+
+    >>> tokenizer = {processor_class}.from_pretrained("{checkpoint}")
+    >>> model = {model_class}.from_pretrained("{checkpoint}")
+    >>> neuron_qa = pipeline("question-answering", model=model, tokenizer=tokenizer)
+
+    >>> question, text = "Who was Jim Henson?", "Jim Henson was a nice puppet"
+    >>> pred = neuron_qa(question, text)
+    ```
+"""
+
+
+@add_start_docstrings(
+    """
+    Neuron Model with a QuestionAnsweringModelOutput for extractive question-answering tasks like SQuAD.
+    """,
+    NEURON_MODEL_START_DOCSTRING,
+)
+class NeuronModelForQuestionAnswering(NeuronModel):
+    """
+    Question Answering model on Neuron devices.
+    """
+
+    auto_model_class = AutoModelForQuestionAnswering
+
+    @add_start_docstrings_to_model_forward(
+        NEURON_TEXT_INPUTS_DOCSTRING.format("batch_size, sequence_length")
+        + QUESTION_ANSWERING_EXAMPLE.format(
+            processor_class=_TOKENIZER_FOR_DOC,
+            model_class="NeuronModelForQuestionAnswering",
+            checkpoint="optimum/roberta-base-squad2",
+        )
+    )
+    def forward(
+        self,
+        input_ids: torch.Tensor = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        token_type_ids: Optional[torch.Tensor] = None,
+        **kwargs,
+    ):
+        neuron_inputs = {
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+        }
+        if token_type_ids is not None:
+            neuron_inputs["token_type_ids"] = token_type_ids
+
+        outputs = self.model(*tuple(neuron_inputs.values()))
+        start_logits = outputs[0]
+        end_logits = outputs[1]
+
+        # converts output to namedtuple for pipelines post-processing
+        return QuestionAnsweringModelOutput(start_logits=start_logits, end_logits=end_logits)
 
 
 SEQUENCE_CLASSIFICATION_EXAMPLE = r"""
@@ -316,3 +400,155 @@ class NeuronModelForSequenceClassification(NeuronModel):
 
         # converts output to namedtuple for pipelines post-processing
         return SequenceClassifierOutput(logits=logits)
+
+
+TOKEN_CLASSIFICATION_EXAMPLE = r"""
+    Example of token classification:
+
+    ```python
+    >>> from transformers import {processor_class}
+    >>> from optimum.neuron import {model_class}
+
+    >>> tokenizer = {processor_class}.from_pretrained("{checkpoint}")
+    >>> model = {model_class}.from_pretrained("{checkpoint}")
+
+    >>> inputs = tokenizer("My name is Philipp and I live in Germany.", return_tensors="pt")
+
+    >>> outputs = model(**inputs)
+    >>> logits = outputs.logits
+    >>> list(logits.shape)
+    [2, 12, 9]
+    ```
+
+    Example using `transformers.pipelines`:
+
+    ```python
+    >>> from transformers import {processor_class}, pipeline
+    >>> from optimum.neuron import {model_class}
+
+    >>> tokenizer = {processor_class}.from_pretrained("{checkpoint}")
+    >>> model = {model_class}.from_pretrained("{checkpoint}")
+    >>> neuron_ner = pipeline("token-classification", model=model, tokenizer=tokenizer)
+
+    >>> text = "My name is Philipp and I live in Germany."
+    >>> pred = neuron_ner(text)
+    ```
+"""
+
+
+@add_start_docstrings(
+    """
+    Neuron Model with a token classification head on top (a linear layer on top of the hidden-states output) e.g.
+    for Named-Entity-Recognition (NER) tasks.
+    """,
+    NEURON_MODEL_START_DOCSTRING,
+)
+class NeuronModelForTokenClassification(NeuronModel):
+    """
+    Token Classification model on Neuron devices.
+    """
+
+    auto_model_class = AutoModelForTokenClassification
+
+    @add_start_docstrings_to_model_forward(
+        NEURON_TEXT_INPUTS_DOCSTRING.format("batch_size, sequence_length")
+        + TOKEN_CLASSIFICATION_EXAMPLE.format(
+            processor_class=_TOKENIZER_FOR_DOC,
+            model_class="NeuronModelForTokenClassification",
+            checkpoint="optimum/bert-base-NER",
+        )
+    )
+    def forward(
+        self,
+        input_ids: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        token_type_ids: Optional[torch.Tensor] = None,
+        **kwargs,
+    ):
+        neuron_inputs = {
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+        }
+        if token_type_ids is not None:
+            neuron_inputs["token_type_ids"] = token_type_ids
+
+        # run inference
+        outputs = self.model(*tuple(neuron_inputs.values()))
+        logits = outputs[0]
+
+        # converts output to namedtuple for pipelines post-processing
+        return TokenClassifierOutput(logits=logits)
+
+
+MULTIPLE_CHOICE_EXAMPLE = r"""
+    Example of mutliple choice:
+
+    ```python
+    >>> from transformers import {processor_class}
+    >>> from optimum.neuron import {model_class}
+
+    >>> tokenizer = {processor_class}.from_pretrained("{checkpoint}")
+    >>> model = {model_class}.from_pretrained("{checkpoint}", export=True)
+
+    >>> num_choices = 4
+    >>> first_sentence = ["Members of the procession walk down the street holding small horn brass instruments."] * num_choices
+    >>> second_sentence = [
+    ...     "A drum line passes by walking down the street playing their instruments.",
+    ...     "A drum line has heard approaching them.",
+    ...     "A drum line arrives and they're outside dancing and asleep.",
+    ...     "A drum line turns the lead singer watches the performance."
+    ... ]
+    >>> inputs = tokenizer(first_sentence, second_sentence, truncation=True, padding=True)
+
+    # Unflatten the inputs values expanding it to the shape [batch_size, num_choices, seq_length]
+    >>> for k, v in inputs.items():
+    ...     inputs[k] = [v[i: i + num_choices] for i in range(0, len(v), num_choices)]
+    >>> inputs = dict(inputs.convert_to_tensors(tensor_type="pt"))
+    >>> outputs = model(**inputs)
+    >>> logits = outputs.logits
+    ```
+"""
+
+
+@add_start_docstrings(
+    """
+    Neuron Model with a multiple choice classification head on top (a linear layer on top of the pooled output and a
+    softmax) e.g. for RocStories/SWAG tasks.
+    """,
+    NEURON_MODEL_START_DOCSTRING,
+)
+class NeuronModelForMultipleChoice(NeuronModel):
+    """
+    Multiple choice model on Neuron devices.
+    """
+
+    auto_model_class = AutoModelForMultipleChoice
+
+    @add_start_docstrings_to_model_forward(
+        NEURON_TEXT_INPUTS_DOCSTRING.format("batch_size, sequence_length")
+        + MULTIPLE_CHOICE_EXAMPLE.format(
+            processor_class=_TOKENIZER_FOR_DOC,
+            model_class="NeuronModelForMultipleChoice",
+            checkpoint="ehdwns1516/bert-base-uncased_SWAG",
+        )
+    )
+    def forward(
+        self,
+        input_ids: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        token_type_ids: Optional[torch.Tensor] = None,
+        **kwargs,
+    ):
+        neuron_inputs = {
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+        }
+        if token_type_ids is not None:
+            neuron_inputs["token_type_ids"] = token_type_ids
+
+        # run inference
+        outputs = self.model(*tuple(neuron_inputs.values()))
+        logits = outputs[0]
+
+        # converts output to namedtuple for pipelines post-processing
+        return MultipleChoiceModelOutput(logits=logits)
