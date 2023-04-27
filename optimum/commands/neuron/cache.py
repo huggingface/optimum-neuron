32437510d17320e42e6649c7c8d277f52f6df8e8
Michael Benayoun <mickbenayoun@gmail.com>
2023-04-27 17:11:06 +0200

    Add documentation

--- optimum/commands/neuron/cache.py
+++ optimum/commands/neuron/cache.py
@@ -52,7 +52,7 @@ class CreateCustomCacheRepoCommand(BaseOptimumCLICommand):
         repo_url = create_custom_cache_repo(repo_id=self.args.name, private=self.args.public)
         public_or_private = "public" if self.args.public else "private"
         logger.info(f"Trainium cache created on the Hugging Face Hub: {repo_url.repo_id} [{public_or_private}].")
-        logger.info(f"Trainium cache name set locally to {self.args.name} in {HF_HOME_CACHE_REPO_FILE}.")
+        logger.info(f"Trainium cache name set locally to {repo_url.repo_id} in {HF_HOME_CACHE_REPO_FILE}.")
 
 
 class SetCustomCacheRepoCommand(BaseOptimumCLICommand):
