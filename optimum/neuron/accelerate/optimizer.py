Fix gradient clipping

optimum/neuron/accelerate/optimizer.py
@@ -14,12 +14,17 @@
 # limitations under the License.
 """Custom AcceleratedOptimizer for Neuron."""
 
+from typing import TYPE_CHECKING, Optional
+
 from accelerate.optimizer import AcceleratedOptimizer
 from accelerate.utils import DistributedType
 
 from ..utils import is_neuronx_distributed_available, is_torch_xla_available
 from .utils.dataclasses import NeuronDistributedType
 
+
+if TYPE_CHECKING:
+    import torch
 
 if is_torch_xla_available():
     import accelerate
@@ -28,13 +33,40 @@ if is_torch_xla_available():
     accelerate.optimizer.xm = xm
 
 if is_neuronx_distributed_available():
-    from neuronx_distributed.parallel_layers import parallel_state
+    from neuronx_distributed import parallel_layers
 
 
 class NeuronAcceleratedOptimizer(AcceleratedOptimizer):
+    def __init__(
+        self,
+        optimizer: "torch.optim.Optimizer",
+        device_placement: bool = True,
+        scaler: Optional["torch.cuda.amp.GradScaler"] = None,
+    ):
+        super().__init__(optimizer, device_placement=device_placement, scaler=scaler)
+
+        self.parameters = []
+        self.parameter_ids = {}
+        self.clip_grad_norm_to_perform = None
+        if self.accelerator_state.distributed_type is NeuronDistributedType.TENSOR_PARALLELISM:
+            self.parameters = [p for group in self.optimizer.param_groups for p in group["params"]]
+            self.parameter_ids = {id(p) for p in self.parameters}
+
     # TODO: might be needed to override this soon.
     def load_state_dict(self, state_dict):
         return super().load_state_dict(state_dict)
+
+    def prepare_clip_grad_norm(self, parameters, max_norm, norm_type=2):
+        # Deferring the clipping to later since gradients need to be reduced first when performing tensor parallelism.
+        # TODO: find a better way to make sure we are using the right parameters for the right optimizer.
+        parameter_ids = {id(p) for p in parameters}
+        if (
+            self.accelerator_state.distributed_type is NeuronDistributedType.TENSOR_PARALLELISM
+            and parameter_ids == self.parameter_ids
+        ):
+            self.clip_grad_norm_to_perform = {"max_norm": max_norm, "norm_type": norm_type}
+        else:
+            raise RuntimeError("The AcceleratedOptimizer handles grad clipping only for tensor parallelism.")
 
     def step(self, closure=None):
         if self.gradient_state.sync_gradients:
@@ -44,11 +76,12 @@ class NeuronAcceleratedOptimizer(AcceleratedOptimizer):
             elif self.accelerator_state.distributed_type is NeuronDistributedType.XLA_FSDP:
                 self.optimizer.step(closure)
             elif self.accelerator_state.distributed_type is NeuronDistributedType.TENSOR_PARALLELISM:
-                xm.optimizer_step(self.optimizer, groups=parallel_state.get_data_parallel_group(as_list=True))
-                # xm.reduce_gradients(self.optimizer, groups=parallel_state.get_data_parallel_group(as_list=True))
-                # parameters = [p for group in self.optimizer.param_groups for p in group["params"]]
-                # parallel_layers.clip_grad_norm(parameters, 1.0, norm_type=2)
-                # self.optimizer.step()
+                xm.reduce_gradients(
+                    self.optimizer, groups=parallel_layers.parallel_state.get_data_parallel_group(as_list=True)
+                )
+                if self.clip_grad_norm_to_perform is not None:
+                    parallel_layers.clip_grad_norm(self.parameters, **self.clip_grad_norm_to_perform)
+                self.optimizer.step()
             elif self.scaler is not None:
                 scale_before = self.scaler.get_scale()
                 self.scaler.step(self.optimizer, closure)