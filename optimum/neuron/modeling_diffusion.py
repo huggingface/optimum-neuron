ce36bab7e95680c25e5093abf3e20e9658dbe4ee
JingyaHuang <jingya@huggingface.co>
2023-08-21 13:42:39 +0000

    address comment

--- optimum/neuron/modeling_diffusion.py
+++ optimum/neuron/modeling_diffusion.py
@@ -78,7 +78,7 @@ class NeuronStableDiffusionPipelineBase(NeuronBaseModel):
         tokenizer: CLIPTokenizer,
         scheduler: Union[DDIMScheduler, PNDMScheduler, LMSDiscreteScheduler],
         feature_extractor: Optional[CLIPFeatureExtractor] = None,
-        device_ids: Optional[List[int]] = [0, 1],
+        device_ids: Optional[List[int]] = None,
         configs: Optional[Dict[str, "PretrainedConfig"]] = None,
         neuron_configs: Optional[Dict[str, "NeuronConfig"]] = None,
         model_save_dir: Optional[Union[str, Path, TemporaryDirectory]] = None,
@@ -104,7 +104,7 @@ class NeuronStableDiffusionPipelineBase(NeuronBaseModel):
                 A scheduler to be used in combination with the U-NET component to denoise the encoded image latents.
             feature_extractor (`Optional[CLIPFeatureExtractor]`, defaults to `None`):
                 A model extracting features from generated images to be used as inputs for the `safety_checker`
-            device_ids (Optional[List[int]], defaults to `[]`):
+            device_ids (Optional[List[int]], defaults to `None`):
                 A list of integers that specify the NeuronCores to use for parallelization
             configs (Optional[Dict[str, "PretrainedConfig"]], defaults to `None`):
                 A dictionary configurations for components of the pipeline.
@@ -235,7 +235,7 @@ class NeuronStableDiffusionPipelineBase(NeuronBaseModel):
         vae_decoder_file_name: Optional[str] = NEURON_FILE_NAME,
         local_files_only: bool = False,
         model_save_dir: Optional[Union[str, Path, TemporaryDirectory]] = None,
-        device_ids: Optional[List[int]] = [0, 1],
+        device_ids: Optional[List[int]] = None,
         **kwargs,  # To share kwargs only available for `_from_transformers`
     ):
         model_id = str(model_id)
@@ -310,7 +310,9 @@ class NeuronStableDiffusionPipelineBase(NeuronBaseModel):
         neuron_configs = {name: cls._neuron_config_init(model_config) for name, model_config in configs.items()}
 
         text_encoder = cls.load_model(model_and_config_save_paths["text_encoder"][0])
-        if device_ids:
+        if device_ids is None:
+            device_ids = [0, 1]
+        if len(device_ids) > 1:
             # Load the compiled UNet onto multiple neuron cores
             unet = torch_neuronx.DataParallel(
                 torch.jit.load(model_and_config_save_paths["unet"][0]),
@@ -361,7 +363,7 @@ class NeuronStableDiffusionPipelineBase(NeuronBaseModel):
         disable_fast_relayout: Optional[bool] = False,
         disable_fallback: bool = False,
         dynamic_batch_size: bool = False,
-        device_ids: Optional[List[int]] = [0, 1],
+        device_ids: Optional[List[int]] = None,
         **kwargs_shapes,
     ) -> "NeuronStableDiffusionPipelineBase":
         if task is None:
