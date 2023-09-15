b90208cc2f514be5082e43c651863b5a3a488617
davidshtian <david881223@outlook.com>
2023-09-15 10:31:30 +0800

    NeuronDecoderConfig from ExportConfig to NeuronConfig

--- optimum/exporters/neuron/base.py
+++ optimum/exporters/neuron/base.py
@@ -346,7 +346,7 @@ class NeuronConfig(ExportConfig, ABC):
             return ModelWrapper(model, list(dummy_inputs.keys()))
 
 
-class NeuronDecoderConfig(ExportConfig):
+class NeuronDecoderConfig(NeuronConfig):
     """
     Base class for configuring the export of Neuron Decoder models
 
