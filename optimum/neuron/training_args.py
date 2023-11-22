5455656a51e99540b86d25c4a6ba56096fc7f4d8
Michael Benayoun <mickbenayoun@gmail.com>
2023-11-22 10:48:14 +0100

    Update training args

--- optimum/neuron/training_args.py
+++ optimum/neuron/training_args.py
@@ -57,8 +57,14 @@ class NeuronTrainingArgumentsMixin:
         default=1, metadata={"help": "The number of replicas the model will be sharded on."}
     )
     disable_embedding_parallelization: bool = field(
-        default=True,
-        metadata={"help": "Whether or not the embedding parallelization when doing TP should be disabled."},
+        default=False,
+        metadata={
+            "help": (
+                "If set, the embeddings will not be parallelized when doing model parallelism. When embeddings are not "
+                "parallelized in decoder and seq2seq models, the language modeling head cannot be parallelized either "
+                "or need an all-gather, which can be costly."
+            )
+        },
     )
     sequence_parallel_enabled: bool = field(
         default=False,
@@ -75,9 +81,6 @@ class NeuronTrainingArgumentsMixin:
     def __post_init__(self):
         # Patches accelerate.utils.imports.is_tpu_available to match `is_torch_xla_available`
         patch_accelerate_is_tpu_available()
-
-        if not self.disable_embedding_parallelization:
-            raise NotImplementedError("Disabling the parallelization of the embeddings is not fully supported yet.")
 
         if self.fsdp != "":
             # Disabling FSDP until next release because it is still very experimental and not validated.
