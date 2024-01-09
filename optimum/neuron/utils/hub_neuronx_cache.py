3444e1736f3875ca7b8e12846af962b3a958c991
David Corvoysier <david@huggingface.co>
2024-01-09 16:17:28 +0000

    feat: add HF Hub neuronx cache proxy

---
+++ optimum/neuron/utils/hub_neuronx_cache.py
@@ -0,0 +1,215 @@
+# coding=utf-8
+# Copyright 2024 The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import logging
+import os
+from contextlib import contextmanager
+from typing import Optional
+
+from huggingface_hub import HfApi, get_token
+
+from ..version import __version__
+from .import_utils import is_neuronx_available
+from .patching import patch_everywhere
+
+
+if is_neuronx_available():
+    from libneuronxla.neuron_cc_cache import (
+        CacheUrl,
+        CompileCache,
+        CompileCacheFs,
+        CompileCacheS3,
+        create_compile_cache,
+    )
+else:
+
+    class CacheUrl:
+        pass
+
+    class CompileCache:
+        pass
+
+    class CompileCacheFs:
+        pass
+
+    class CompileCacheS3:
+        pass
+
+    def create_compile_cache():
+        pass
+
+
+logger = logging.getLogger(__name__)
+
+
+class CompileCacheHfProxy(CompileCache):
+    """A HuggingFace Hub proxy cache implementing the CompileCache API
+
+    This cache first looks for compilation artifacts into the default cache, then the
+    specified HuggingFace cache repository.
+
+    Args:
+        repo_id (`str`):
+            The id of the HuggingFace cache repository, in the form 'org|user/name'.
+        default_cache (`CompileCache`):
+            The default neuron compiler cache (can be either a local file or S3 cache).
+        endpoint (`Optional[str]`):
+            The HuggingFaceHub endpoint: only required for unit tests to switch to the staging Hub.
+        token (`Optional[str]`):
+            The HuggingFace token to use to fetch/push artifacts. If not specified it will correspond
+            to the current user token.
+    """
+
+    cache_type = "hf"
+
+    def __init__(
+        self, repo_id: str, default_cache: CompileCache, endpoint: Optional[str] = None, token: Optional[str] = None
+    ):
+        # Initialize the proxy cache as expected by the parent class
+        super().__init__(default_cache.cache_url)
+        self.cache_path = default_cache.cache_path
+        # Initialize specific members
+        self.default_cache = default_cache
+        self.api = HfApi(endpoint=endpoint, token=token, library_name="optimum-neuron", library_version=__version__)
+        # Check if the HF cache id is valid
+        if not self.api.repo_exists(repo_id):
+            raise ValueError(f"The {repo_id} repository does not exist or you don't have access to it.")
+        self.repo_id = repo_id
+
+    def get_cache_dir(self, model_hash, compile_flags_str):
+        return self.default_cache.get_cache_dir(model_hash, compile_flags_str)
+
+    def clean(self):
+        # Should we clean the Hf Hub cache also ?
+        self.default_cache.clean()
+
+    def clear_locks(self):
+        # Clear locks in the default cache only, as the Hf already deals with concurrency
+        self.default_cache.clear_locks()
+
+    def get_hlos(self, failed_neff_str=""):
+        return self.default_cache.get_hlos(failed_neff_str)
+
+    def hlo_acquire_lock(self, h):
+        # Put a lock in the default cache only, as the Hf already deals with concurrency
+        return self.default_cache.hlo_acquire_lock(h)
+
+    def hlo_release_lock(self, h):
+        # Release lock in the default cache only, as the Hf already deals with concurrency
+        return self.default_cache.hlo_release_lock(h)
+
+    def remove(self, path):
+        # Only remove in the default cache
+        return self.default_cache.remove(path)
+
+    def _rel_path(self, path):
+        # Remove the default cache url from the path
+        if path.startswith(self.default_cache.cache_path):
+            return path[len(self.default_cache.cache_path) :].lstrip("/")
+
+    def exists(self, path):
+        # Always prioritize the default cache
+        if self.default_cache.exists(path):
+            return True
+        return self.api.file_exists(self.repo_id, self._rel_path(path))
+
+    def download_file(self, filename, dst_path):
+        # Always prioritize the default cache for faster retrieval
+        if self.default_cache.exists(filename):
+            self.default_cache.download_file(filename, dst_path)
+        else:
+            rel_filename = self._rel_path(filename)
+            local_path = self.api.hf_hub_download(self.repo_id, rel_filename)
+            os.symlink(local_path, dst_path)
+            logger.info(f"Fetched cached {rel_filename} from {self.repo_id}")
+
+    def synchronize(self):
+        if isinstance(self.default_cache, CompileCacheS3):
+            raise ValueError("HuggingFace hub compiler cache synchronization is not supported for S3.")
+        logger.info(f"Synchronizing {self.repo_id} Hub cache with {self.default_cache.cache_path} local cache")
+        self.api.upload_folder(
+            repo_id=self.repo_id,
+            folder_path=self.default_cache.cache_path,
+            commit_message="Synchronizing local compiler cache.",
+            ignore_patterns="lock",
+        )
+        logger.info("Synchronization complete.")
+
+    def upload_file(self, cache_path, src_path):
+        # Only upload to the default cache: use synchronize to populate the Hub cache
+        self.default_cache.upload_file(cache_path, src_path)
+
+    def upload_string_to_file(self, cache_path, data):
+        # Only upload to the default cache: use synchronize to populate the Hub cache
+        self.default_cache.upload_string_to_file(cache_path, data)
+
+    def download_file_to_string(self, filename, limit=None):
+        # Always prioritize the default cache for faster retrieval
+        if self.default_cache.exists(filename):
+            return self.default_cache.download_file_to_string(filename, limit)
+        rel_filename = self._rel_path(filename)
+        local_path = self.api.hf_hub_download(self.repo_id, rel_filename)
+        with open(local_path, "rb") as f:
+            s = f.read().decode(errors="replace")
+        logger.info(f"Fetched cached {rel_filename} from {self.repo_id}")
+        return s
+
+
+def get_hub_cache():
+    HUB_CACHE = "aws-neuron/optimum-neuron-cache"
+    return os.getenv("CUSTOM_CACHE_REPO", HUB_CACHE)
+
+
+def _create_hub_compile_cache_proxy(
+    cache_url: Optional[CacheUrl] = None,
+    cache_repo_id: Optional[str] = None,
+):
+    if cache_url is None:
+        cache_url = CacheUrl.get_cache_url()
+    if cache_repo_id is None:
+        cache_repo_id = get_hub_cache()
+    default_cache = CompileCacheS3(cache_url) if cache_url.is_s3() else CompileCacheFs(cache_url)
+    # Reevaluate endpoint and token (needed for tests altering the environment)
+    endpoint = os.getenv("HF_ENDPOINT")
+    token = get_token()
+    return CompileCacheHfProxy(cache_repo_id, default_cache, endpoint=endpoint, token=token)
+
+
+@contextmanager
+def hub_neuronx_cache():
+    """A context manager to trigger the HuggingFace Hub proxy compiler cache"""
+    if not is_neuronx_available():
+        raise ImportError("Neuronx compiler is not avilable: please reinstall optimum-neuron[neuronx]")
+
+    def hf_create_compile_cache(cache_url):
+        return _create_hub_compile_cache_proxy(cache_url)
+
+    try:
+        patch_everywhere("create_compile_cache", hf_create_compile_cache, "libneuronxla")
+        yield
+    finally:
+        patch_everywhere("create_compile_cache", create_compile_cache, "libneuronxla")
+
+
+def synchronize_hub_cache(cache_repo_id: Optional[str] = None):
+    """Synchronize the neuronx compiler cache with the optimum-neuron hub cache.
+
+    Args:
+        repo_id (`Optional[str]`):
+            The id of the HuggingFace cache repository, in the form 'org|user/name'.
+    """
+    if not is_neuronx_available():
+        raise ImportError("Neuronx compiler is not avilable: please reinstall optimum-neuron[neuronx]")
+    hub_cache_proxy = _create_hub_compile_cache_proxy(cache_repo_id=cache_repo_id)
+    hub_cache_proxy.synchronize()
---
+++ tests/cache/test_neuronx_cache.py
@@ -0,0 +1,143 @@
+# coding=utf-8
+# Copyright 2024 The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import glob
+import os
+import shutil
+import socket
+from tempfile import TemporaryDirectory
+
+import pytest
+import torch
+from huggingface_hub import HfApi
+from transformers.testing_utils import ENDPOINT_STAGING
+
+from optimum.neuron import NeuronModelForCausalLM
+from optimum.neuron.utils import hub_neuronx_cache, synchronize_hub_cache
+from optimum.neuron.utils.testing_utils import is_inferentia_test, requires_neuronx
+from optimum.utils.testing_utils import TOKEN
+
+
+@pytest.fixture
+def cache_repos():
+    # Setup: create temporary Hub repository and local cache directory
+    api = HfApi(endpoint=ENDPOINT_STAGING, token=TOKEN)
+    user = api.whoami()["name"]
+    hostname = socket.gethostname()
+    cache_repo_id = f"{user}/{hostname}-optimum-neuron-cache"
+    if api.repo_exists(cache_repo_id):
+        api.delete_repo(cache_repo_id)
+    cache_repo_id = api.create_repo(cache_repo_id).repo_id
+    cache_dir = TemporaryDirectory()
+    cache_path = cache_dir.name
+    # Modify environment to force neuronx cache to use temporary caches
+    previous_env = {}
+    env_vars = ["NEURON_COMPILE_CACHE_URL", "CUSTOM_CACHE_REPO", "HF_ENDPOINT", "HF_TOKEN"]
+    for var in env_vars:
+        previous_env[var] = os.environ.get(var)
+    os.environ["NEURON_COMPILE_CACHE_URL"] = cache_path
+    os.environ["CUSTOM_CACHE_REPO"] = cache_repo_id
+    os.environ["HF_ENDPOINT"] = ENDPOINT_STAGING
+    os.environ["HF_TOKEN"] = TOKEN
+    yield (cache_path, cache_repo_id)
+    # Teardown
+    api.delete_repo(cache_repo_id)
+    for var in env_vars:
+        if previous_env[var] is None:
+            os.environ.pop(var)
+        else:
+            os.environ[var] = previous_env[var]
+
+
+def export_decoder_model(model_id):
+    batch_size = 2
+    sequence_length = 512
+    num_cores = 1
+    auto_cast_type = "fp32"
+    return NeuronModelForCausalLM.from_pretrained(
+        model_id,
+        export=True,
+        batch_size=batch_size,
+        sequence_length=sequence_length,
+        num_cores=num_cores,
+        auto_cast_type=auto_cast_type,
+    )
+
+
+def check_decoder_generation(model):
+    batch_size = model.config.neuron["batch_size"]
+    input_ids = torch.ones((batch_size, 20), dtype=torch.int64)
+    with torch.inference_mode():
+        sample_output = model.generate(input_ids)
+        assert sample_output.shape[0] == batch_size
+
+
+def get_local_cached_files(cache_path):
+    return glob.glob(f"{cache_path}/**/*/*.*", recursive=True)
+
+
+def assert_local_and_hub_cache_sync(cache_path, cache_repo_id):
+    api = HfApi(endpoint=ENDPOINT_STAGING, token=TOKEN)
+    remote_files = api.list_repo_files(cache_repo_id)
+    local_files = get_local_cached_files(cache_path)
+    for file in local_files:
+        assert os.path.isfile(file)
+        path_in_repo = file[len(cache_path) :].lstrip("/")
+        assert path_in_repo in remote_files
+
+
+def local_cache_size(cache_path):
+    return len(get_local_cached_files(cache_path))
+
+
+@is_inferentia_test
+@requires_neuronx
+def test_decoder_cache(cache_repos):
+    cache_path, cache_repo_id = cache_repos
+    # Export the model a first time to populate the local cache
+    model = export_decoder_model("hf-internal-testing/tiny-random-gpt2")
+    check_decoder_generation(model)
+    # Synchronize the hub cache with the local cache
+    synchronize_hub_cache(cache_repo_id=cache_repo_id)
+    assert_local_and_hub_cache_sync(cache_path, cache_repo_id)
+    # Clear the local cache
+    for root, dirs, files in os.walk(cache_path):
+        for f in files:
+            os.unlink(os.path.join(root, f))
+        for d in dirs:
+            shutil.rmtree(os.path.join(root, d))
+    assert local_cache_size(cache_path) == 0
+    # Export the model again: the compilation artifacts should be fetched from the Hub
+    with hub_neuronx_cache():
+        model = export_decoder_model("hf-internal-testing/tiny-random-gpt2")
+    check_decoder_generation(model)
+    # Verify the local cache directory has not been populated
+    assert local_cache_size(cache_path) == 0
+
+
+@is_inferentia_test
+@requires_neuronx
+def test_decoder_cache_wrong_url():
+    repo_id = "foo/bar"
+    previous_hub_cache = os.getenv("CUSTOM_CACHE_REPO")
+    os.environ["CUSTOM_CACHE_REPO"] = repo_id
+    try:
+        with pytest.raises(ValueError, match=f"The {repo_id} repository does not exist"):
+            with hub_neuronx_cache():
+                export_decoder_model("hf-internal-testing/tiny-random-gpt2")
+    finally:
+        if previous_hub_cache is None:
+            os.environ.pop("CUSTOM_CACHE_REPO")
+        else:
+            os.environ["OPTIMUM_NEURON_HUB_CACHE"] = previous_hub_cache
