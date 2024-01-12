dce046cbeba6e4527717231e51623eb67cd2f1c9
Michael Benayoun <mickbenayoun@gmail.com>
2024-01-12 15:14:16 +0100

    Fix test

--- tests/test_cache_utils.py
+++ tests/test_cache_utils.py
@@ -342,6 +342,7 @@ class StagingNeuronUtilsTestCase(StagingTestMixin, TestCase):
         _test_list_in_registry(True)
 
 
+@is_trainium_test
 class NeuronHashTestCase(TestCase):
     def test_neuron_hash_is_not_mutable(self):
         bert_model = BertModel(BertConfig())
