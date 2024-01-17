68a852b2f5dea2a27e535f3bb30bd1a35adfde0f
David Corvoysier <david@huggingface.co>
2024-01-17 09:20:36 +0000

    refactor(decoder): prepare to exclude checkpoints from serialization

--- optimum/neuron/modeling_decoder.py
+++ optimum/neuron/modeling_decoder.py
@@ -21,7 +21,6 @@ from pathlib import Path
 from tempfile import TemporaryDirectory
 from typing import TYPE_CHECKING, Optional, Tuple, Union
 
-import torch
 from huggingface_hub import HfApi, get_token, snapshot_download
 from huggingface_hub.utils import is_google_colab
 from transformers import AutoConfig, AutoModel, GenerationConfig
@@ -34,7 +33,6 @@ from .utils.version_utils import check_compiler_compatibility, get_neuronxcc_ver
 
 
 if is_transformers_neuronx_available():
-    from transformers_neuronx.module import PretrainedModel as NeuronxPretrainedModel
     from transformers_neuronx.module import save_split
 
 
@@ -71,17 +69,22 @@ class NeuronDecoderModel(OptimizedModel):
 
     def __init__(
         self,
-        model: torch.nn.Module,
         config: "PretrainedConfig",
-        model_path: Union[str, Path, TemporaryDirectory],
+        checkpoint_dir: Union[str, Path, TemporaryDirectory],
+        compiled_dir: Optional[Union[str, Path, TemporaryDirectory]] = None,
         generation_config: Optional[GenerationConfig] = None,
     ):
-        if not is_transformers_neuronx_available() or not isinstance(model, NeuronxPretrainedModel):
-            raise ValueError("The source model must be a transformers_neuronx.PreTrainedModel.")
+        neuron_config = getattr(config, "neuron", None)
+        if neuron_config is None:
+            raise ValueError(
+                "The specified model is not a neuron model. "
+                "Please convert your model to neuron format by passing export=True."
+            )
 
-        super().__init__(model, config)
-        self.model_path = model_path
+        self.checkpoint_dir = checkpoint_dir
+        self.compiled_dir = compiled_dir
         if generation_config is None:
+            logger.info("Generation config file not found, using a generation config created from the model config.")
             generation_config = GenerationConfig.from_model_config(config)
         self.generation_config = generation_config
         # Registers the NeuronModelForXXX classes into the transformers AutoModel classes to avoid warnings when creating
@@ -90,11 +93,45 @@ class NeuronDecoderModel(OptimizedModel):
         if hasattr(self.auto_model_class, "register"):
             self.auto_model_class.register(AutoConfig, self.__class__)
 
+        # Evaluate the configuration passed during export
+        task = neuron_config["task"]
+        batch_size = neuron_config["batch_size"]
+        sequence_length = neuron_config["sequence_length"]
+        num_cores = neuron_config["num_cores"]
+        auto_cast_type = neuron_config["auto_cast_type"]
+
+        check_compiler_compatibility(neuron_config["compiler_type"], neuron_config["compiler_version"])
+
+        exporter = get_exporter(config, task)
+
+        # transformers-neuronx uses f32/f16 instead of fp32/fp16
+        auto_cast_type = auto_cast_type.replace("p", "")
+        checkpoint_path = checkpoint_dir.name if isinstance(checkpoint_dir, TemporaryDirectory) else checkpoint_dir
+        neuronx_model = exporter.neuronx_class.from_pretrained(
+            checkpoint_path,
+            batch_size=batch_size,
+            n_positions=sequence_length,
+            tp_degree=num_cores,
+            amp=auto_cast_type,
+        )
+
+        if compiled_dir is not None:
+            # Specify the path where compiled artifacts are stored before conversion
+            neuronx_model.load(compiled_dir)
+
+        # Compile the Neuron model (if present compiled artifacts will be reloaded instead of compiled)
+        neuron_cc_flags = os.environ.get("NEURON_CC_FLAGS", "")
+        os.environ["NEURON_CC_FLAGS"] = neuron_cc_flags + " --model-type=transformer"
+        with hub_neuronx_cache():
+            neuronx_model.to_neuron()
+        os.environ["NEURON_CC_FLAGS"] = neuron_cc_flags
+
+        super().__init__(neuronx_model, config)
+
     @classmethod
-    def _from_transformers(
+    def _create_checkpoint(
         cls,
         model_id: str,
-        config: "PretrainedConfig",
         use_auth_token: Optional[Union[bool, str]] = None,
         revision: Optional[str] = None,
         force_download: bool = False,
@@ -103,18 +140,8 @@ class NeuronDecoderModel(OptimizedModel):
         local_files_only: bool = False,
         trust_remote_code: bool = False,
         task: Optional[str] = None,
-        batch_size: Optional[int] = 1,
-        sequence_length: Optional[int] = None,
-        num_cores: Optional[int] = 2,
-        auto_cast_type: Optional[str] = "fp32",
         **kwargs,
-    ) -> "NeuronDecoderModel":
-        if not is_transformers_neuronx_available():
-            raise ModuleNotFoundError("The transformers_neuronx package is required to export the model.")
-
-        if task is None:
-            task = TasksManager.infer_task_from_model(cls.auto_model_class)
-
+    ) -> TemporaryDirectory:
         # Instantiate the transformers model checkpoint
         model = TasksManager.get_model_from_task(
             task=task,
@@ -135,6 +162,32 @@ class NeuronDecoderModel(OptimizedModel):
         model.save_pretrained(
             checkpoint_dir.name, save_function=save_split, safe_serialization=False, max_shard_size="10000GB"
         )
+        return checkpoint_dir
+
+    @classmethod
+    def _from_transformers(
+        cls,
+        model_id: str,
+        config: "PretrainedConfig",
+        task: Optional[str] = None,
+        batch_size: Optional[int] = 1,
+        sequence_length: Optional[int] = None,
+        num_cores: Optional[int] = 2,
+        auto_cast_type: Optional[str] = "fp32",
+        **kwargs,
+    ) -> "NeuronDecoderModel":
+        if not is_transformers_neuronx_available():
+            raise ModuleNotFoundError("The transformers_neuronx package is required to export the model.")
+
+        if task is None:
+            task = TasksManager.infer_task_from_model(cls.auto_model_class)
+
+        # Instantiate the transformers model checkpoint
+        checkpoint_dir = cls._create_checkpoint(
+            model_id,
+            task=task,
+            **kwargs,
+        )
 
         # If the sequence_length was not specified, deduce it from the model configuration
         if sequence_length is None:
@@ -152,37 +205,30 @@ class NeuronDecoderModel(OptimizedModel):
             "compiler_version": get_neuronxcc_version(),
         }
 
-        return cls._from_pretrained(checkpoint_dir, config)
+        # Try to reload the generation config (if any)
+        generation_config = None
+        try:
+            generation_config = GenerationConfig.from_pretrained(model_id)
+        except OSError:
+            pass
+
+        return cls(config, checkpoint_dir, generation_config=generation_config)
 
     @classmethod
-    def _get_neuron_paths(
-        cls, model_dir: Union[str, Path, TemporaryDirectory], token: Optional[str] = None
-    ) -> Tuple[str, str, str]:
-        if isinstance(model_dir, TemporaryDirectory):
-            model_path = model_dir.name
-            # We are in the middle of an export: the checkpoint is in the temporary model directory
-            checkpoint_path = model_path
-            # There are no compiled artifacts yet
-            compiled_path = None
-        else:
-            # The model has already been exported
-            if os.path.isdir(model_dir):
-                model_path = model_dir
-            else:
-                # Download the neuron model from the Hub
-                model_path = snapshot_download(model_dir, token=token)
-            # The checkpoint is in a subdirectory
-            checkpoint_path = os.path.join(model_path, cls.CHECKPOINT_DIR)
-            # So are the compiled artifacts
-            compiled_path = os.path.join(model_path, cls.COMPILED_DIR)
-        return model_path, checkpoint_path, compiled_path
+    def _get_neuron_dirs(cls, model_path: Union[str, Path]) -> Tuple[str, str]:
+        # The checkpoint is in a subdirectory
+        checkpoint_dir = os.path.join(model_path, cls.CHECKPOINT_DIR)
+        # So are the compiled artifacts
+        compiled_dir = os.path.join(model_path, cls.COMPILED_DIR)
+        return checkpoint_dir, compiled_dir
 
     @classmethod
     def _from_pretrained(
         cls,
-        model_id: Union[str, Path, TemporaryDirectory],
+        model_id: Union[str, Path],
         config: "PretrainedConfig",
         use_auth_token: Optional[str] = None,
+        revision: Optional[str] = None,
         **kwargs,
     ) -> "NeuronDecoderModel":
         # Verify we are actually trying to load a neuron model
@@ -192,69 +238,47 @@ class NeuronDecoderModel(OptimizedModel):
                 "The specified directory does not contain a neuron model. "
                 "Please convert your model to neuron format by passing export=True."
             )
-
-        # Evaluate the configuration passed during export
-        task = neuron_config["task"]
-        batch_size = neuron_config["batch_size"]
-        sequence_length = neuron_config["sequence_length"]
-        num_cores = neuron_config["num_cores"]
-        auto_cast_type = neuron_config["auto_cast_type"]
-
         check_compiler_compatibility(neuron_config["compiler_type"], neuron_config["compiler_version"])
 
-        exporter = get_exporter(config, task)
+        model_path = model_id
+        if not os.path.isdir(model_id):
+            model_path = snapshot_download(model_id, token=use_auth_token, revision=revision)
 
-        model_path, checkpoint_path, compiled_path = cls._get_neuron_paths(model_id, use_auth_token)
-
-        # transformers-neuronx uses f32/f16 instead of fp32/fp16
-        auto_cast_type = auto_cast_type.replace("p", "")
-        neuronx_model = exporter.neuronx_class.from_pretrained(
-            checkpoint_path,
-            batch_size=batch_size,
-            n_positions=sequence_length,
-            tp_degree=num_cores,
-            amp=auto_cast_type,
-        )
-
-        if compiled_path is not None:
-            # Specify the path where compiled artifacts are stored before conversion
-            neuronx_model.load(compiled_path)
-
-        # Compile the Neuron model (if present compiled artifacts will be reloaded instead of compiled)
-        neuron_cc_flags = os.environ.get("NEURON_CC_FLAGS", "")
-        os.environ["NEURON_CC_FLAGS"] = neuron_cc_flags + " --model-type=transformer"
-        with hub_neuronx_cache():
-            neuronx_model.to_neuron()
-        os.environ["NEURON_CC_FLAGS"] = neuron_cc_flags
+        checkpoint_dir, compiled_dir = cls._get_neuron_dirs(model_path)
+        assert os.path.isdir(checkpoint_dir)
+        assert os.path.isdir(compiled_dir)
 
         # Try to reload the generation config (if any)
         generation_config = None
         try:
-            generation_config = GenerationConfig.from_pretrained(model_path)
+            generation_config = GenerationConfig.from_pretrained(model_id)
         except OSError:
-            logger.info("Generation config file not found, using a generation config created from the model config.")
+            pass
 
-        return cls(neuronx_model, config, model_id, generation_config)
+        return cls(config, checkpoint_dir, compiled_dir=compiled_dir, generation_config=generation_config)
 
     def forward(self, *args, **kwargs):
         raise NotImplementedError()
 
     def _save_pretrained(self, save_directory: Union[str, Path]):
-        _, src_chkpt_path, src_compiled_path = self._get_neuron_paths(self.model_path)
-        _, dst_chkpt_path, dst_compiled_path = self._get_neuron_paths(save_directory)
+        dst_checkpoint_path, dst_compiled_path = self._get_neuron_dirs(save_directory)
 
-        shutil.copytree(src_chkpt_path, dst_chkpt_path)
+        def copy_dir_to_path(src_dir: Union[str, Path, TemporaryDirectory], dst_path: Union[str, Path]):
+            if isinstance(src_dir, TemporaryDirectory):
+                shutil.copytree(src_dir.name, dst_path)
+            elif not os.path.samefile(src_dir, dst_path):
+                os.symlink(dst_path, src_dir)
 
-        if src_compiled_path is None:
-            # The compiled model has never been serialized: do it now
+        # Copy checkpoint directory (it always exists)
+        copy_dir_to_path(self.checkpoint_dir, dst_checkpoint_path)
+        self.checkpoint_dir = dst_checkpoint_path
+        # Save or create compiled directory
+        if self.compiled_dir is None:
+            # The compilation artifacts have never been saved, do it now
             self.model.save(dst_compiled_path)
         else:
-            shutil.copytree(src_compiled_path, dst_compiled_path)
-
-        if isinstance(self.model_path, TemporaryDirectory):
-            # Let temporary directory go out-of-scope to release disk space
-            self.model_path = save_directory
-
+            copy_dir_to_path(self.compiled_dir, dst_compiled_path)
+        self.compiled_dir = dst_compiled_path
         self.generation_config.save_pretrained(save_directory)
 
     def push_to_hub(
