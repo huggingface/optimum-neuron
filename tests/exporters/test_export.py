d943b7e9d64a654289833c7e4cf0722b22d13fad
Jingya HUANG <44135271+JingyaHuang@users.noreply.github.com>
2024-01-25 21:37:40 +0000

    fix test

--- tests/exporters/test_export.py
+++ tests/exporters/test_export.py
@@ -65,7 +65,7 @@ def _get_models_to_test(
     models_to_test = []
     for model_type, model_names_tasks in export_models_dict.items():
         model_type = model_type.replace("_", "-")
-        if model_type not in exclude_model_types:
+        if exclude_model_types is None or (model_type not in exclude_model_types):
             task_config_mapping = TasksManager.get_supported_tasks_for_model_type(model_type, "neuron")
 
             if isinstance(model_names_tasks, str):  # test export of all tasks on the same model
