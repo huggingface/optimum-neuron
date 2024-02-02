ed1e04f5ca1bcfec0da8818d3f92420f8610f8ea
Jingya HUANG <44135271+JingyaHuang@users.noreply.github.com>
2024-02-02 13:42:44 +0000

    fix setup

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except Exception as error:
 
 
 INSTALL_REQUIRES = [
-    "transformers ~= 4.37.2",
+    "transformers == 4.37.*",
     "accelerate == 0.23.0",
     "optimum >= 1.16.2",
     "huggingface_hub >= 0.20.1",
@@ -62,7 +62,7 @@ EXTRAS_REQUIRE = {
         "torchvision==0.14.*",
         "neuronx_distributed==0.6.0",
     ],
-    "diffusers": ["diffuser ~= 0.26.1"],
+    "diffusers": ["diffuser == 0.26.*"],
     "sentence-transformers": ["sentence-transformers >= 2.2.0"],
 }
 
