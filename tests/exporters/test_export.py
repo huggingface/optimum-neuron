00ac6ce26e187f9753793ea0a60493781482ffbb
Jingya HUANG <44135271+JingyaHuang@users.noreply.github.com>
2024-02-12 09:45:58 +0100

    Fix optimum-cli export for inf1  (#474)
    * fix + test
    
    * improve tests
    
    ---------
    
    Co-authored-by: JingyaHuang <jingya@huggingface.co>

--- tests/exporters/test_export.py
+++ tests/exporters/test_export.py
@@ -170,7 +170,6 @@ class NeuronExportTestCase(unittest.TestCase):
         _get_models_to_test(EXPORT_MODELS_TINY, exclude_model_types=WEIGHTS_NEFF_SEPARATION_UNSUPPORTED_ARCH)
     )
     @is_inferentia_test
-    @requires_neuronx
     def test_export_separated_weights(self, test_name, name, model_name, task, neuron_config_constructor):
         self._neuronx_export(
             test_name, name, model_name, task, neuron_config_constructor, inline_weights_to_neff=False
