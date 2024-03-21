eac7f830670a9db9cb5542b80e1278d294490534
Michael Benayoun <mickbenayoun@gmail.com>
2024-03-21 14:12:07 +0100

    Rename

--- optimum/neuron/distributed/base.py
+++ optimum/neuron/distributed/base.py
@@ -500,7 +500,7 @@ class Parallelizer(ABC):
 
     @classmethod
     @requires_neuronx_distributed
-    def _initialize_for_parallel_compilation(
+    def _initialize_for_precompilation(
         cls,
         model: "PreTrainedModel",
         names_of_the_parameters_to_consider: Set[str],
@@ -696,7 +696,7 @@ class Parallelizer(ABC):
             logger.info("Loading and initializing the weights, this might take a while on large models.")
 
         if is_precompilation():
-            cls._initialize_for_parallel_compilation(model, names_of_the_parameters_to_consider)
+            cls._initialize_for_precompilation(model, names_of_the_parameters_to_consider)
         else:
             if skip_linear_weight_load:
                 # Load the weights to the parallel linears if the loading was skipped during parallelization.
