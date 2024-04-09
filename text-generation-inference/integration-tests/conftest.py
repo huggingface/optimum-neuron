bb1cc964ebe136184ea73e93663320383ffbc125
oOraph <13552058+oOraph@users.noreply.github.com>
2024-04-09 11:04:27 +0200

    Set up tgi environment values with the ones used to build the model (#529)
    * Set up tgi environment values with the ones used to build the model
    
    Need this to workaround the model static params, for the docker entrypoint to adapt tgi environment accordingly to the specified model
    This will make usage of the image easier: default params (e.g not specifying anything) should be enough for most models
    
    Signed-off-by: Raphael Glon <oOraph@users.noreply.github.com>
    
    * fixes
    
    Signed-off-by: Raphael Glon <oOraph@users.noreply.github.com>
    
    * fixes
    
    Signed-off-by: Raphael Glon <oOraph@users.noreply.github.com>
    
    * minor: logging
    
    Signed-off-by: Raphael Glon <oOraph@users.noreply.github.com>
    
    * Integration tests for inf2 + tgi_env wrapper
    
    Signed-off-by: Raphael Glon <oOraph@users.noreply.github.com>
    
    * Github ci worklow
    
    Signed-off-by: Raphael Glon <oOraph@users.noreply.github.com>
    
    * To run on github ci we cannot share a volume but need to embed the model within an image built on the flight
    
    Signed-off-by: Raphael Glon <oOraph@users.noreply.github.com>
    
    * More flexible on expected outputs
    
    Signed-off-by: Raphael Glon <oOraph@users.noreply.github.com>
    
    * Be more flexible about compiler version
    
    Signed-off-by: Raphael Glon <oOraph@users.noreply.github.com>
    
    * Refacto
    
    bump dev version,
    use a single workflow for TGI,
    simplify a bit the implicit env test
    
    Signed-off-by: Raphael Glon <oOraph@users.noreply.github.com>
    
    * Misc fixes
    
    Signed-off-by: Raphael Glon <oOraph@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Raphael Glon <oOraph@users.noreply.github.com>
    Co-authored-by: Raphael Glon <oOraph@users.noreply.github.com>

--- text-generation-inference/integration-tests/conftest.py
+++ text-generation-inference/integration-tests/conftest.py
@@ -1,12 +1,13 @@
 import asyncio
 import contextlib
+import logging
 import os
 import random
-import shlex
-import subprocess
+import re
+import string
 import sys
+import tempfile
 import time
-from tempfile import TemporaryDirectory
 from typing import List
 
 import docker
@@ -17,9 +18,10 @@ from text_generation import AsyncClient
 from text_generation.types import Response
 
 
+LOG = logging.getLogger(__file__)
 DOCKER_IMAGE = os.getenv("DOCKER_IMAGE", "neuronx-tgi:latest")
 HUGGING_FACE_HUB_TOKEN = os.getenv("HUGGING_FACE_HUB_TOKEN", None)
-DOCKER_VOLUME = os.getenv("DOCKER_VOLUME", "/data")
+DOCKER_LOCAL_DIR_PATTERN = re.compile(r"^/data/(.*)$")
 
 
 class LauncherHandle:
@@ -71,15 +73,7 @@ def event_loop():
 
 
 @pytest.fixture(scope="module")
-def data_volume():
-    tmpdir = TemporaryDirectory()
-    yield tmpdir.name
-    # Cleanup the temporary directory using sudo as it contains root files created by the container
-    subprocess.run(shlex.split(f"sudo rm -rf {tmpdir.name}"))
-
-
-@pytest.fixture(scope="module")
-def launcher(event_loop, data_volume):
+def launcher(event_loop):
     @contextlib.contextmanager
     def docker_launcher(
         model_id: str,
@@ -112,17 +106,43 @@ def launcher(event_loop, data_volume):
             if var in os.environ:
                 env[var] = os.environ[var]
 
-        volumes = [f"{data_volume}:/data"]
+        # Workaround to bypass docker dind issues preventing to share a volume from the container running tests
+        # to another
+        m = DOCKER_LOCAL_DIR_PATTERN.match(model_id)
+        if m:
+            local_dir = model_id
+            real_model_id = m.group(1)
+            docker_content = f"""
+            FROM {DOCKER_IMAGE}
+            RUN huggingface-cli download --local-dir {local_dir} {real_model_id}
+            """
+
+            docker_tag = "awesome-workaround:{}".format(
+                "".join(random.choice(string.ascii_lowercase + string.digits) for _ in range(5))
+            )
+            LOG.info(
+                "Building image on the flight derivated from %s, tagged with %s",
+                DOCKER_IMAGE,
+                docker_tag,
+            )
+            with tempfile.NamedTemporaryFile() as f:
+                f.write(docker_content.encode("utf-8"))
+                f.flush()
+                image, logs = client.images.build(path=".", dockerfile=f.name, tag=docker_tag)
+            LOG.info("Successfully built image %s", image.id)
+            LOG.debug("Build logs %s", logs)
+        else:
+            docker_tag = DOCKER_IMAGE
+            image = None
 
         container = client.containers.run(
-            DOCKER_IMAGE,
+            docker_tag,
             command=args,
             name=container_name,
             environment=env,
             auto_remove=False,
             detach=True,
             devices=["/dev/neuron0"],
-            volumes=volumes,
             ports={"80/tcp": port},
             shm_size="1G",
         )
@@ -130,15 +150,26 @@ def launcher(event_loop, data_volume):
         yield ContainerLauncherHandle(client, container.name, port)
 
         try:
-            container.stop()
-            container.wait()
-        except NotFound:
-            pass
+            try:
+                container.stop()
+                container.wait()
+            except NotFound:
+                pass
+            container_output = container.logs().decode("utf-8")
+            print(container_output, file=sys.stderr)
 
-        container_output = container.logs().decode("utf-8")
-        print(container_output, file=sys.stderr)
-
-        container.remove()
+            container.remove()
+        finally:
+            # Cleanup the build image
+            if image:
+                LOG.info("Cleaning image %s", image.id)
+                try:
+                    image.remove(force=True)
+                except NotFound:
+                    pass
+                except Exception as e:
+                    LOG.error("Error while removing image %s, skiping", image.id)
+                    LOG.exception(e)
 
     return docker_launcher
 
