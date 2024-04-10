951bf17fa91294bb08da84fcb6a1c192645d7003
Michael Benayoun <mickbenayoun@gmail.com>
2024-04-10 16:17:30 +0200

    Sync with accelerate done

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except Exception as error:
 
 INSTALL_REQUIRES = [
     "transformers == 4.36.2",
-    "accelerate == 0.23.0",
+    "accelerate == 0.29.2",
     "optimum ~= 1.18.0",
     "huggingface_hub >= 0.20.1",
     "numpy>=1.22.2, <=1.25.2",
