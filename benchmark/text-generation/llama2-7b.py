c8f15f9bba2f43742d86034c95721d61bda8705c
David Corvoysier <david@huggingface.co>
2024-04-12 11:45:44 +0200

    Modify benchmarks (#563)
    * feat(benchmark): evaluate throughput on output tokens only
    
    * feat(benchmark): add mistralv2
    
    * docs(benchmark): update Llama-7b results
    
    * docs(benchmark): add mistral v0.2 benchmark
    
    * docs(benchmark): update llama2-13b benchmark
    
    * fix: wrap bench in main()
    
    * fix(bench): be careful with initial batch size

--- benchmark/text-generation/llama2-7b.py
+++ benchmark/text-generation/llama2-7b.py
@@ -1,33 +1,43 @@
-import os
 from tempfile import TemporaryDirectory
 
 from transformers import AutoTokenizer
 
 from benchmark import run
 from optimum.neuron import NeuronModelForCausalLM
+from optimum.neuron.modeling_decoder import get_available_cores
 
 
-model_configurations = {
-    "Llama-2-7B-BS1": ["meta-llama/Llama-2-7b-chat-hf", 1, 4096],
-    "Llama-2-7B-BS4": ["meta-llama/Llama-2-7b-chat-hf", 4, 4096],
-}
+def main():
+    NUM_CORES = 24
+    num_cores = get_available_cores()
+    if num_cores < NUM_CORES:
+        raise ValueError(f"This benchmark can only run on an instance with at least {NUM_CORES} cores.")
 
-num_cores = len(os.listdir("/sys/class/neuron_device/")) * 2
-if num_cores >= 4:
-    extra_model_configurations = {
+    model_configurations = {
+        "Llama-2-7B-BS1": ["meta-llama/Llama-2-7b-chat-hf", 1, 4096],
+        "Llama-2-7B-BS4": ["meta-llama/Llama-2-7b-chat-hf", 4, 4096],
         "Llama-2-7B-BS8": ["meta-llama/Llama-2-7b-chat-hf", 8, 4096],
         "Llama-2-7B-BS16": ["meta-llama/Llama-2-7b-chat-hf", 16, 4096],
+        "Llama-2-7B-BS32": ["meta-llama/Llama-2-7b-chat-hf", 32, 4096],
     }
-    model_configurations = {**model_configurations, **extra_model_configurations}
 
-for model_name, model_configuration in model_configurations.items():
-    model_id, batch_size, seq_length = model_configuration
-    model = NeuronModelForCausalLM.from_pretrained(
-        model_id, export=True, batch_size=batch_size, sequence_length=seq_length, auto_cast_type="fp16"
-    )
-    with TemporaryDirectory() as tmpdir:
-        model.save_pretrained(tmpdir)
-        tokenizer = AutoTokenizer.from_pretrained(model_id)
-        tokenizer.save_pretrained(tmpdir)
-        json_path = f"{model_name}.json"
-        run(tmpdir, 256, 1024, json_path=json_path)
+    for model_name, model_configuration in model_configurations.items():
+        model_id, batch_size, seq_length = model_configuration
+        model = NeuronModelForCausalLM.from_pretrained(
+            model_id,
+            export=True,
+            batch_size=batch_size,
+            sequence_length=seq_length,
+            auto_cast_type="fp16",
+            num_cores=NUM_CORES,
+        )
+        with TemporaryDirectory() as tmpdir:
+            model.save_pretrained(tmpdir)
+            tokenizer = AutoTokenizer.from_pretrained(model_id)
+            tokenizer.save_pretrained(tmpdir)
+            json_path = f"{model_name}.json"
+            run(tmpdir, 256, 2048, json_path=json_path)
+
+
+if __name__ == "__main__":
+    main()
