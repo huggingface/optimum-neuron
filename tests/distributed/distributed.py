ee4678c50b0eb72844e11521b27866e03babbe2b
Michael Benayoun <mickbenayoun@gmail.com>
2024-04-12 12:26:30 +0200

    Remove XRT server related code

--- tests/distributed/distributed.py
+++ tests/distributed/distributed.py
@@ -18,7 +18,6 @@
 # https://github.com/microsoft/DeepSpeed/blob/master/tests/unit/common.py
 
 import inspect
-import multiprocessing
 import os
 import socket
 import time
@@ -26,7 +25,6 @@ import uuid
 from abc import ABC, abstractmethod
 from typing import List, Union
 
-import psutil
 import pytest
 import torch
 import torch.distributed as dist
@@ -179,12 +177,10 @@ class DistributedExec(ABC):
             pytest.exit("Test hanged, exiting", returncode=0)
         except Exception as e:
             self._close_pool(pool, num_procs)
-            self._terminate_xrt_server()
             raise e
         finally:
             # Tear down distributed environment and close process pools
             self._close_pool(pool, num_procs)
-            self._terminate_xrt_server()
 
         # If we skipped a test, propagate that to this process
         if any(skip_msgs):
@@ -247,30 +243,6 @@ class DistributedExec(ABC):
                 pool.join()
             except ValueError:
                 pass
-
-    def _terminate_xrt_server(self):
-        xrt_server_str = "torch_neuronx.distributed._xrt_run_server"
-        startmethod = mp.get_start_method(allow_none=True)
-        # Rules:
-        # - `startmethod is None`: the XRT server tracks pytest's PID.
-        # - `startmethod="spawn"`: the parent process of the pool's processes is pytest, so the XRT server tracks
-        # pytest's PID.
-        # - `startmethod="fork"`: same as `startmethod="spawn"`.
-        # - `startmethod="forkserver"`: the parent process of the pool's processes is the forkserver, so the XRT server tracks
-        # the forkserver's PID.
-        if startmethod == "forkserver":
-            target_pid = multiprocessing.forkserver._forkserver._forkserver_pid
-        else:
-            target_pid = os.getpid()
-
-        for p in psutil.process_iter():
-            try:
-                if "python3" in p.name() and len(p.cmdline()) == 7:
-                    cmdline = p.cmdline()
-                    if cmdline[2] == xrt_server_str and cmdline[-1] == str(target_pid):
-                        p.terminate()
-            except psutil.ZombieProcess:
-                continue
 
 
 class DistributedTest(DistributedExec):
