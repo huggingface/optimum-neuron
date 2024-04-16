17cf1478234c8b0c82620eb286fe36504ec206c7
Michael Benayoun <mickbenayoun@gmail.com>
2024-04-16 14:20:54 +0200

    Update Llama attention

--- optimum/neuron/distributed/decoder_models.py
+++ optimum/neuron/distributed/decoder_models.py
@@ -15,7 +15,7 @@
 """Classes related to `neuronx-distributed` to perform parallelism."""
 
 import warnings
-from typing import TYPE_CHECKING, Any, Callable, List, Optional, Tuple
+from typing import TYPE_CHECKING, Callable, Optional, Tuple
 
 import torch
 from transformers.cache_utils import Cache
@@ -455,6 +455,7 @@ class LlamaSequenceParallelismSpecs(SequenceParallelismSpecs):
             past_key_value: Optional[Cache] = None,
             output_attentions: bool = False,
             use_cache: bool = False,
+            cache_position: Optional[torch.LongTensor] = None,
             **kwargs,
         ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
             if "padding_mask" in kwargs:
@@ -502,45 +503,29 @@ class LlamaSequenceParallelismSpecs(SequenceParallelismSpecs):
                 key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
                 value_states = value_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
 
-            kv_seq_len = key_states.shape[-2]
-            if past_key_value is not None:
-                if self.layer_idx is None:
-                    raise ValueError(
-                        "The cache structure has changed since version `transformers v4.36. If you are using "
-                        f"{self.__class__.__name__} for auto-regressive decoding with k/v caching, please make sure to "
-                        "initialize the attention class with a layer index."
-                    )
-                kv_seq_len += past_key_value.get_usable_length(kv_seq_len, self.layer_idx)
-            cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
-            query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
+            past_key_value = getattr(self, "past_key_value", past_key_value)
+            cos, sin = self.rotary_emb(value_states, position_ids)
+            query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
 
             if past_key_value is not None:
-                cache_kwargs = {"sin": sin, "cos": cos}  # Specific to RoPE models
+                # sin and cos are specific to RoPE models; cache_position needed for the static cache
+                cache_kwargs = {"sin": sin, "cos": cos, "cache_position": cache_position}
                 key_states, value_states = past_key_value.update(
                     key_states, value_states, self.layer_idx, cache_kwargs
                 )
 
-            # repeat k/v heads if n_kv_heads < n_heads
             key_states = repeat_kv(key_states, self.num_key_value_groups)
             value_states = repeat_kv(value_states, self.num_key_value_groups)
 
             attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(self.head_dim)
 
-            if attn_weights.size() != (bsz, self.num_heads, q_len, kv_seq_len):
-                raise ValueError(
-                    f"Attention weights should be of size {(bsz, self.num_heads, q_len, kv_seq_len)}, but is"
-                    f" {attn_weights.size()}"
-                )
-
-            if attention_mask is not None:
-                if attention_mask.size() != (bsz, 1, q_len, kv_seq_len):
-                    raise ValueError(
-                        f"Attention mask should be of size {(bsz, 1, q_len, kv_seq_len)}, but is {attention_mask.size()}"
-                    )
-                attn_weights = attn_weights + attention_mask
+            if attention_mask is not None:  # no matter the length, we just slice it
+                causal_mask = attention_mask[:, :, :, : key_states.shape[-2]]
+                attn_weights = attn_weights + causal_mask
 
             # upcast attention to fp32
             attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
+            attn_weights = nn.functional.dropout(attn_weights, p=self.attention_dropout, training=self.training)
             attn_output = torch.matmul(attn_weights, value_states)
 
             if attn_output.size() != (bsz, self.num_heads, q_len, self.head_dim):
@@ -580,19 +565,6 @@ class LlamaPipelineParallelismSpecs(PipelineParallelismSpecs):
     TRASNFORMER_LAYER_CLS = LlamaDecoderLayer
     DEFAULT_INPUT_NAMES = ("input_ids", "attention_mask", "labels")
     LEAF_MODULE_CLASSES_NAMES = [LlamaRMSNorm]
-
-    @classmethod
-    def get_patching_specs(cls) -> List[Tuple[str, Any]]:
-        return []
-        # leaf_prepare_4d_causal_attention_mask = torch.fx._symbolic_trace._create_wrapped_func(
-        #     _prepare_4d_causal_attention_mask
-        # )
-        # return [
-        #     (
-        #         "transformers.models.llama.modeling_llama._prepare_4d_causal_attention_mask",
-        #         leaf_prepare_4d_causal_attention_mask,
-        #     ),
-        # ]
 
 
 class LlamaParallelizer(Parallelizer):
