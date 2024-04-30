5cd6eb07095b1f376ef4f223d5a8e1f77a059801
Jingya HUANG <44135271+JingyaHuang@users.noreply.github.com>
2024-04-30 22:20:11 +0000

    decorator

--- tests/inference/test_modeling.py
+++ tests/inference/test_modeling.py
@@ -50,11 +50,9 @@ from optimum.neuron import (
 )
 from optimum.neuron.utils import NEURON_FILE_NAME, is_neuron_available, is_neuronx_available
 from optimum.neuron.utils.testing_utils import is_inferentia_test, requires_neuronx
-from optimum.utils import (
-    CONFIG_NAME,
-    logging,
-)
+from optimum.utils import CONFIG_NAME, logging
 
+from ..test_utils import require_hf_token
 from .inference_utils import (
     MODEL_NAMES,
     SEED,
@@ -68,6 +66,7 @@ logger = logging.get_logger()
 
 
 @is_inferentia_test
+@require_hf_token
 class NeuronModelIntegrationTest(NeuronModelIntegrationTestMixin):
     MODEL_ID = MODEL_NAMES["bert"]
     if is_neuron_available():
