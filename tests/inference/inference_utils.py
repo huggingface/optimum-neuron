9d6e6d464a9f1abe778975289c43c9d74e84744a
Michael Benayoun <mickbenayoun@gmail.com>
2024-06-05 18:09:49 +0200

    Fix

--- tests/inference/inference_utils.py
+++ tests/inference/inference_utils.py
@@ -62,7 +62,7 @@ SENTENCE_TRANSFORMERS_MODEL_NAMES = {
 
 
 class NeuronModelIntegrationTestMixin(unittest.TestCase):
-    USER = "optimum-internal-tesing"
+    USER = "optimum-internal-testing"
     MODEL_ID = None
     NEURON_MODEL_REPO = None
     NEURON_MODEL_CLASS = None
