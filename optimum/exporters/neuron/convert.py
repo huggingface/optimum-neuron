1931162322be5ef3d8f60aa2e7a31ee676a51330
Jingya HUANG <44135271+JingyaHuang@users.noreply.github.com>
2024-06-21 13:59:45 +0000

    add compiler args for controlnet export

--- optimum/exporters/neuron/convert.py
+++ optimum/exporters/neuron/convert.py
@@ -591,11 +591,11 @@ def add_stable_diffusion_compiler_args(config, compiler_args):
     identifier = getattr(config._config, "_name_or_path", "") + " " + getattr(config._config, "_class_name", "")
     identifier = identifier.lower()
 
-    sd_components = ["text_encoder", "vae", "vae_encoder", "vae_decoder"]
+    sd_components = ["text_encoder", "vae", "vae_encoder", "vae_decoder", "controlnet"]
     if any(component in identifier for component in sd_components):
         compiler_args.append("--enable-fast-loading-neuron-binaries")
-    # unet
-    if "unet" in identifier:
+    # unet or controlnet
+    if "unet" in identifier or "controlnet" in identifier:
         # SDXL unet doesn't support fast loading neuron binaries
         if not getattr(config, "is_sdxl", False):
             compiler_args.append("--enable-fast-loading-neuron-binaries")
@@ -607,11 +607,11 @@ def improve_stable_diffusion_loading(config, neuron_model):
     # Combine the model name and its path to identify which is the subcomponent in Stable Diffusion pipeline
     identifier = getattr(config._config, "_name_or_path", "") + " " + getattr(config._config, "_class_name", "")
     identifier = identifier.lower()
-    sd_components = ["text_encoder", "unet", "vae", "vae_encoder", "vae_decoder"]
+    sd_components = ["text_encoder", "unet", "vae", "vae_encoder", "vae_decoder", "controlnet"]
     if any(component in identifier for component in sd_components):
         neuronx.async_load(neuron_model)
     # unet
-    if "unet" in identifier:
+    if "unet" in identifier or "controlnet" in identifier:
         neuronx.lazy_load(neuron_model)
 
 
