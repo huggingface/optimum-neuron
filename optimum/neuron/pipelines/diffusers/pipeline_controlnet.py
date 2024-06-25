020ccb6c8204ac301a54f415186e802dd74a8968
Jingya HUANG <44135271+JingyaHuang@users.noreply.github.com>
2024-06-25 10:18:41 +0000

    correct comment

--- optimum/neuron/pipelines/diffusers/pipeline_controlnet.py
+++ optimum/neuron/pipelines/diffusers/pipeline_controlnet.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Override some diffusers API for StableDiffusionControlNetPipeline"""
+"""Override some diffusers API for NeuronStableDiffusionControlNetPipeline"""
 
 import logging
 from typing import Any, Callable, Dict, List, Optional, Union
