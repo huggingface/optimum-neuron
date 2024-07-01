1a522018cbf9f19ac56de9e27d78ac3d6d9bb58c
David Corvoysier <david@huggingface.co>
2024-07-01 08:59:28 +0000

    tests(decoder): add mixtral config

--- tests/decoder/conftest.py
+++ tests/decoder/conftest.py
@@ -37,6 +37,10 @@ DECODER_MODEL_CONFIGURATIONS = {
         "model_id": "optimum/mistral-1.1b-testing",
         "export_kwargs": {"batch_size": 4, "sequence_length": 4096, "num_cores": 2, "auto_cast_type": "bf16"},
     },
+    "mixtral": {
+        "model_id": "dacorvo/Mixtral-tiny",
+        "export_kwargs": {"batch_size": 4, "sequence_length": 1024, "num_cores": 2, "auto_cast_type": "fp16"},
+    },
 }
 
 
