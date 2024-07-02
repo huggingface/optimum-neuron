789727d1239e29aa2a9ee46bbfb413f2ee4efee9
Michael Benayoun <mickbenayoun@gmail.com>
2024-07-02 18:39:07 +0200

    Remove old code

--- optimum/neuron/trainers.py
+++ optimum/neuron/trainers.py
@@ -353,7 +353,6 @@ class AugmentTrainerForNeuronMixin:
         return inputs
 
     def compute_loss(self, model, inputs, return_outputs: bool = False):
-        self.state.last_inputs = inputs
         from neuronx_distributed.pipeline import NxDPPModel
 
         if isinstance(model, NxDPPModel):
@@ -407,8 +406,6 @@ class AugmentTrainerForNeuronMixin:
         ignore_keys: Optional[List[str]] = None,
     ) -> Tuple[Optional[torch.Tensor], Optional[torch.Tensor], Optional[torch.Tensor]]:
         from neuronx_distributed.pipeline import NxDPPModel
-
-        self.state.last_inputs = inputs
 
         if isinstance(model, NxDPPModel):
             if not prediction_loss_only:
