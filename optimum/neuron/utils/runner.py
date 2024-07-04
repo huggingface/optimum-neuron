839dd8538ca64e9e2dd97d3ea5ea65420cf93489
Michael Benayoun <mickbenayoun@gmail.com>
2024-07-04 17:04:08 +0200

    Change dataset

--- optimum/neuron/utils/runner.py
+++ optimum/neuron/utils/runner.py
@@ -125,12 +125,11 @@ class ExampleRunner:
             "task_name": "sst2",
         },
         "token-classification": {
-            "dataset_name": "conll2003",
+            "dataset_name": "bnsapa/cybersecurity-ner",
             "set_max_length": True,
             "extra_command_line_arguments": [
                 "--pad_to_max_length",
                 "--ignore_mismatched_sizes",
-                "--trust_remote_code",
             ],
         },
         "multiple-choice": {
