156e24bcf6b37390ac33a37dc7db4b9e6cb733f9
Michael Benayoun <mickbenayoun@gmail.com>
2024-07-09 17:58:21 +0200

    Fix issues

--- optimum/neuron/distributed/decoder_models.py
+++ optimum/neuron/distributed/decoder_models.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 """Classes related to `neuronx-distributed` to perform parallelism."""
 
-from typing import TYPE_CHECKING, Callable, Optional, Tuple
+from typing import TYPE_CHECKING, Callable, Optional
 
 import torch
 from transformers.models.gpt_neo.modeling_gpt_neo import GPTNeoBlock, GPTNeoSelfAttention
@@ -197,89 +197,14 @@ class GPTNeoXSequenceParallelismSpecs(SequenceParallelismSpecs):
         if not sequence_parallel_enabled:
             return
 
-        from transformers.models.gpt_neox.modeling_gpt_neox import apply_rotary_pos_emb
-
-        def sequence_parallel_forward(
-            self,
-            hidden_states: torch.FloatTensor,
-            attention_mask: torch.FloatTensor,
-            position_ids: torch.LongTensor,
-            head_mask: Optional[torch.FloatTensor] = None,
-            layer_past: Optional[Tuple[torch.Tensor]] = None,
-            use_cache: Optional[bool] = False,
-            output_attentions: Optional[bool] = False,
-        ):
-            has_layer_past = layer_past is not None
-
-            # Compute QKV
-            # If sequence_parallel_enabled:
-            #   --> [seq_len, batch, (num_heads * 3 * head_size)]
-            # Else:
-            #   --> [batch, seq_len, (num_heads * 3 * head_size)]
-            qkv = self.query_key_value(hidden_states)
-
-            # If sequence_parallel_enabled:
-            #   --> [seq_len, batch, num_heads, 3 * head_size]
-            # Else:
-            #   --> [batch, seq_len, num_heads, 3 * head_size]
-            new_qkv_shape = qkv.size()[:-1] + (self.num_attention_heads, 3 * self.head_size)
-            qkv = qkv.view(*new_qkv_shape)
-
-            if sequence_parallel_enabled:
-                # [seq_len, batch, num_attention_heads, 3 * head_size] --> 3 [batch, num_attention_heads, seq_len, head_size]
-                query = qkv[..., : self.head_size].permute(1, 2, 0, 3)
-                key = qkv[..., self.head_size : 2 * self.head_size].permute(1, 2, 0, 3)
-                value = qkv[..., 2 * self.head_size :].permute(1, 2, 0, 3)
-            else:
-                # [batch, seq_len, num_attention_heads, 3 * head_size] --> 3 [batch, num_attention_heads, seq_len, head_size]
-                query = qkv[..., : self.head_size].permute(0, 2, 1, 3)
-                key = qkv[..., self.head_size : 2 * self.head_size].permute(0, 2, 1, 3)
-                value = qkv[..., 2 * self.head_size :].permute(0, 2, 1, 3)
-
-            # Compute rotary embeddings on rotary_ndims
-            query_rot = query[..., : self.rotary_ndims]
-            query_pass = query[..., self.rotary_ndims :]
-            key_rot = key[..., : self.rotary_ndims]
-            key_pass = key[..., self.rotary_ndims :]
-
-            # Compute token offset for rotary embeddings (when decoding)
-            seq_len = key.shape[-2]
-            if has_layer_past:
-                seq_len += layer_past[0].shape[-2]
-            cos, sin = self.rotary_emb(value, seq_len=seq_len)
-            query, key = apply_rotary_pos_emb(query_rot, key_rot, cos, sin, position_ids)
-            query = torch.cat((query, query_pass), dim=-1)
-            key = torch.cat((key, key_pass), dim=-1)
-
-            # Cache QKV values
-            if has_layer_past:
-                past_key = layer_past[0]
-                past_value = layer_past[1]
-                key = torch.cat((past_key, key), dim=-2)
-                value = torch.cat((past_value, value), dim=-2)
-            present = (key, value) if use_cache else None
-
-            # Compute attention
-            attn_output, attn_weights = self._attn(query, key, value, attention_mask, head_mask)
-
-            # Reshape outputs
-            if sequence_parallel_enabled:
-                # [batch, num_attention_heads, seq_len, head_size] -> [seq_len, batch, hidden_size]
-                attn_output = attn_output.permute(2, 0, 1, 3).contiguous()
-                attn_output = attn_output.view(*attn_output.shape[:2], -1)
-            else:
-                attn_output = self._merge_heads(attn_output, self.num_attention_heads, self.head_size)
-            attn_output = self.dense(attn_output)
-
-            outputs = (attn_output, present)
-            if output_attentions:
-                outputs += (attn_weights,)
-
-            return outputs
-
         for module in model.modules():
             if isinstance(module, GPTNeoXAttention):
-                module.forward = sequence_parallel_forward.__get__(module)
+                if not isinstance(module, NeuronAttention):
+                    raise ValueError(
+                        "The gpt neox model has not been prepared by the NeuronPreparator. It is required for sequence "
+                        "parallelism."
+                    )
+                module.sequence_parallel_enabled = sequence_parallel_enabled
 
 
 class GPTNeoXParallelizer(Parallelizer):
@@ -432,7 +357,7 @@ class LlamaSequenceParallelismSpecs(SequenceParallelismSpecs):
             if isinstance(module, LlamaAttention):
                 if not isinstance(module, NeuronAttention):
                     raise ValueError(
-                        "The llama model has not been prepare by the NeuronPreparator. It is required for sequence "
+                        "The llama model has not been prepared by the NeuronPreparator. It is required for sequence "
                         "parallelism."
                     )
                 module.sequence_parallel_enabled = sequence_parallel_enabled
@@ -608,7 +533,7 @@ class MistralSequenceParallelismSpecs(SequenceParallelismSpecs):
             if isinstance(module, MistralAttention):
                 if not isinstance(module, NeuronAttention):
                     raise ValueError(
-                        "The mistral model has not been prepare by the NeuronPreparator. It is required for sequence "
+                        "The mistral model has not been prepared by the NeuronPreparator. It is required for sequence "
                         "parallelism."
                     )
                 module.sequence_parallel_enabled = sequence_parallel_enabled
