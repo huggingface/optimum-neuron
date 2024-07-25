b890d115254e7588fc60f8cf0e9f4080cc1a13b3
Michael Benayoun <mickbenayoun@gmail.com>
2024-07-25 18:15:08 +0200

    Support resize embeddings

--- optimum/neuron/trainers.py
+++ optimum/neuron/trainers.py
@@ -907,7 +907,7 @@ class AugmentTrainerForNeuronMixin:
         self.control = self.callback_handler.on_train_begin(args, self.state, self.control)
 
         # Mark step before training to materialize any tensor before creating the training graph.
-        xm.mark_step()
+        # xm.mark_step()
 
         # Skip the first epochs_trained epochs to get the random state of the dataloader at the right point.
         if not args.ignore_data_skip:
