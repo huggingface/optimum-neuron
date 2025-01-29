50a82292ac54f602045a7a35192647be799fbac5
David Corvoysier <david@huggingface.co>
2025-01-29 13:01:48 +0000

    fix(ami): use new AWS venv path

--- infrastructure/ami/scripts/install-huggingface-libraries.sh
+++ infrastructure/ami/scripts/install-huggingface-libraries.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Activate the neuron virtual environment
-source /opt/aws_neuronx_venv_pytorch_2_1/bin/activate
+source /opt/aws_neuronx_venv_pytorch/bin/activate
 
 echo "Step: install-hugging-face-libraries"
 
@@ -15,12 +15,12 @@ pip install --upgrade --no-cache-dir \
     "markupsafe==2.1.1" \
     "jinja2==3.1.2" \
     "attrs==23.1.0" \
-    "hf_transfer>=0.1.4" 
+    "hf_transfer>=0.1.4"
 
 # Temporary fix for the issue: https://github.com/huggingface/optimum-neuron/issues/142
 pip install -U optimum
 echo 'export PATH="${HOME}/.local/bin:$PATH"' >> "${HOME}/.bashrc"
-# Add HF_TRANSFER by default 
+# Add HF_TRANSFER by default
 echo 'export HF_HUB_ENABLE_HF_TRANSFER=1' >> "${HOME}/.bashrc"
 
 echo "Step: install-and-copy-optimum-neuron-examples"
