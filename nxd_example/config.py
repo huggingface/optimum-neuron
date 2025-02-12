7c8a71fe16573407f04759ec5648acfe2931bf23
David Corvoysier <david@huggingface.co>
2025-02-12 17:25:14 +0100

    importing nxd example

---
+++ nxd_example/config.py
@@ -0,0 +1,30 @@
+import torch 
+from dataclasses import dataclass
+
+@dataclass
+class Config():
+    vocab_size: int
+    hidden_size: int
+    n_layers: int
+    n_heads: int
+    n_kv_heads: int
+    head_dim: int
+    intermediate_size: int
+    dtype: torch.dtype = torch.float32
+    rms_norm_eps: int = 1e-05
+    rope_theta: int = 500000.0
+    pad_token: int = 0
+    
+# LLama 1B    
+Llama1B = Config(vocab_size = 128256,
+                hidden_size = 2048,
+                n_layers = 16,
+                n_heads = 32,
+                n_kv_heads = 8,
+                head_dim = 64,
+                intermediate_size = 8192,
+                dtype = torch.bfloat16,
+                rms_norm_eps = 1e-05,
+                rope_theta = 500000.0,
+                pad_token = 128004
+            )
