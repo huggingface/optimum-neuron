e4864a5715a20e80346aa3b1f626e4d4ea54eecb
JingyaHuang <huang_jingya@outlook.com>
2025-04-09 12:20:05 +0000

    apply suggestion

--- optimum/neuron/models/__init__.py
+++ optimum/neuron/models/__init__.py
@@ -12,14 +12,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-from . import (
-    bert,
-    clip,
-    granite,
-    llama,
-    phi3,
-    qwen2,
-    whisper,
-    yolos,
-)
