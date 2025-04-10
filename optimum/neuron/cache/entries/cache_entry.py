bad926173911f6657f6b61e3a176d4b102a384f2
David Corvoysier <david@huggingface.co>
2025-04-10 13:25:43 +0000

    fix(cache): remove task for multi-models
    The task is None for multi model cache entries since we cache the whole
    pipeline and not a single combination of sub-models corresponding to
    one of the tasks.

--- optimum/neuron/cache/entries/cache_entry.py
+++ optimum/neuron/cache/entries/cache_entry.py
@@ -59,19 +59,11 @@ class ModelCacheEntry:
     def to_dict(self) -> Dict[str, Any]:
         raise NotImplementedError
 
-    @classmethod
-    def from_dict(cls, model_id: str, task: str, configs: Dict[str, Any]) -> "ModelCacheEntry":
-        raise NotImplementedError
-
     @property
     def neuron_config(self) -> Dict[str, Any]:
         raise NotImplementedError
 
     def has_same_arch(self, other: "ModelCacheEntry"):
-        raise NotImplementedError
-
-    @classmethod
-    def from_hub(cls, model_id: str, task: str):
         raise NotImplementedError
 
     # Generic methods relying on the API above
@@ -91,7 +83,7 @@ class ModelCacheEntry:
         if api.file_exists(model_id, "config.json"):
             return SingleModelCacheEntry.from_hub(model_id, task)
         elif api.file_exists(model_id, "model_index.json"):
-            return MultiModelCacheEntry.from_hub(model_id, task)
+            return MultiModelCacheEntry.from_hub(model_id)
         raise ValueError(f"Unable to evaluate model type for {model_id}: is it a diffusers or transformers model ?")
 
     def serialize(self) -> str:
@@ -114,5 +106,5 @@ class ModelCacheEntry:
         elif entry_class == "MultiModelCacheEntry":
             from .multi_model import MultiModelCacheEntry
 
-            return MultiModelCacheEntry.from_dict(model_id, task, cache_dict)
+            return MultiModelCacheEntry.from_dict(model_id, cache_dict)
         raise ValueError(f"Invalid cache entry of type {entry_class}")
