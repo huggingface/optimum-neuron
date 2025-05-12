420569b2f3bdddabc9a2388ca32ea96fba7d0e6f
Alvaro Moran <alvaro.moran@huggingface.co>
2025-05-12 12:46:26 +0000

    test(granite): added overfit test for granite
    The overfit test has been adapted to accept more configuration settings.
    Note that the expected loss does not go down to 0, but it descends. The
    test then just checks that the last loss value is below a given target.

--- tests/distributed/test_train_overfit.py
+++ tests/distributed/test_train_overfit.py
@@ -25,7 +25,10 @@ if is_neuronx_distributed_available():
 def _overfit_causal_lm(
     model_class_name,
     model_name_or_path,
+    learning_rate,
+    warmup_ratio,
     training_kwargs,
+    max_expected_loss,
     tp_size,
     pp_size,
     output_dir,
@@ -55,8 +58,8 @@ def _overfit_causal_lm(
         pipeline_parallel_size=pp_size,
         do_train=True,
         do_eval=False,
-        learning_rate=1e-4,
-        warmup_ratio=0.03,
+        learning_rate=learning_rate,
+        warmup_ratio=warmup_ratio,
         per_device_train_batch_size=1,
         gradient_accumulation_steps=16,
         lr_scheduler_type="cosine",
@@ -97,7 +100,7 @@ def _overfit_causal_lm(
     trainer.train()
 
     # The master worker checks the logs, since it is the only worker to have access to them, to retrieve the last logged
-    # loss. It then checks if it is equal to 0.0.
+    # loss. It then checks if it is lower or equal to max_expected_loss.
     if xr.global_ordinal() == 0:
         last_loss = None
         for logs in reversed(stored_logs):
@@ -107,21 +110,35 @@ def _overfit_causal_lm(
         if last_loss is None:
             raise ValueError("No loss found in the logs.")
         print("Last loss", last_loss)
-        assert last_loss == 0.0, "The model did not overfit the dataset."
+        assert last_loss <= max_expected_loss, "The model did not overfit the dataset."
 
 
 @pytest.mark.parametrize(
-    "model_class_name,model_name_or_path,training_kwargs",
+    "model_class_name,model_name_or_path,learning_rate,warmup_ratio,training_kwargs, max_expected_loss",
     [
         [
             "LlamaForCausalLM",
             "meta-llama/Llama-3.2-1B-Instruct",
+            1e-4,
+            0.03,
             {
                 "use_flash_attention": True,
             },
+            0.0,
+        ],
+        [
+            "GraniteForCausalLM",
+            "ibm-granite/granite-3.2-2b-instruct",
+            2e-3,
+            0,
+            {
+                "use_flash_attention": False,
+                "disable_sequence_parallel": True,
+            },
+            0.07,
         ],
     ],
-    ids=["meta-llama/Llama-3.2-1B-Instruct"],
+    ids=["meta-llama/Llama-3.2-1B-Instruct", "ibm-granite/granite-3.2-2b-instruct"],
 )
 @pytest.mark.parametrize(
     "world_size,tp_size,pp_size",
@@ -131,7 +148,10 @@ def _overfit_causal_lm(
 def test_overfit_causal_lm(
     model_class_name,
     model_name_or_path,
+    learning_rate,
+    warmup_ratio,
     training_kwargs,
+    max_expected_loss,
     world_size,
     tp_size,
     pp_size,
@@ -142,7 +162,10 @@ def test_overfit_causal_lm(
         _overfit_causal_lm,
         model_class_name,
         model_name_or_path,
+        learning_rate,
+        warmup_ratio,
         training_kwargs,
+        max_expected_loss,
         tp_size,
         pp_size,
         tmpdir,
