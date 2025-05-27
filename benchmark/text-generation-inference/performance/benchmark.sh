01b6b92a8a8139392eb9d1adc159d5910a71c56f
Jim Burtoft <39492751+jimburtoft@users.noreply.github.com>
2025-05-26 21:56:07 -0400

    Update benchmark.sh
    updating for the new version of guidellm 
    
    parameter name changes and some formatting
    
    also fixing default model path

--- benchmark/text-generation-inference/performance/benchmark.sh
+++ benchmark/text-generation-inference/performance/benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-model=${1:-meta-llama/Meta-Llama-3.1-8B-Instruct}
+model=${1:-meta-llama/Llama-3.1-8B-Instruct}
 
 date_str=$(date '+%Y-%m-%d-%H-%M-%S')
 output_path="${model//\//_}#${date_str}_guidellm_report.json"
@@ -11,9 +11,10 @@ export GUIDELLM__NUM_SWEEP_PROFILES=1
 export GUIDELLM__MAX_CONCURRENCY=128
 export GUIDELLM__REQUEST_TIMEOUT=60
 
-guidellm \
+guidellm benchmark \
   --target "http://localhost:8080/v1" \
   --model ${model} \
-  --data-type emulated \
-  --data "prompt_tokens=1500,prompt_tokens_variance=150,generated_tokens=250,generated_tokens_variance=20" \
+  --rate-type sweep \
+  --data "prompt_tokens=1500,prompt_tokens_variance=150,output_tokens=250,outpu
+t_tokens_variance=20" \
   --output-path ${output_path} \
