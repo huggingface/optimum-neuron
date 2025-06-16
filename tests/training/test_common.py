1d9ed2f1e1b341b634ddcd4fdd016710475e8337
Michael Benayoun <mickbenayoun@gmail.com>
2025-06-16 17:17:09 +0200

    [WIP] huge refactoring

--- tests/training/test_common.py
+++ tests/training/test_common.py
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""General tests related to distributed training."""
 
 from collections import defaultdict
 from pathlib import Path
