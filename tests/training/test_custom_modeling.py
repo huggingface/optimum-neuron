7690bf8f943137e0ef38e2aba5e69ecd3878874c
Michael Benayoun <mickbenayoun@gmail.com>
2025-06-20 14:09:09 +0200

    Revert to smollm

--- tests/training/test_custom_modeling.py
+++ tests/training/test_custom_modeling.py
@@ -233,7 +233,7 @@ def _custom_model_matches_original_model(
 
 @pytest.mark.parametrize("qkv_implementation", ["regular_qkv", "fuse_qkv", "qkv_linear"])
 @pytest.mark.parametrize("model_specs", CUSTOM_MODELINGS_TO_TEST, ids=[specs[0] for specs in CUSTOM_MODELINGS_TO_TEST])
-@pytest.mark.timeout(120)  # If a test takes more that 2 minutes, it is likely stuck.
+@pytest.mark.timeout(300)  # If a test takes more that 5 minutes, it is likely stuck.
 @is_trainium_test
 def test_custom_modeling_matches_original(
     model_specs,
--- tests/training/test_overfit.py
+++ tests/training/test_overfit.py
@@ -225,8 +225,8 @@ def _overfit_causal_lm(
             50,
         ],
         [
-            "GPT2LMHeadModel",
-            "distilbert/distilgpt2",
+            "LlamaForCausalLM",
+            "HuggingFaceTB/SmolLM2-135M-Instruct",
             False,
             1e-4,
             0.04,
@@ -241,7 +241,7 @@ def _overfit_causal_lm(
         "meta-llama/Llama-3.2-1B-Instruct",
         "ibm-granite/granite-3.2-2b-instruct",
         "Qwen/Qwen3-0.6B",
-        "distilbert/distilgpt2",
+        "HuggingFaceTB/SmolLM2-135M-Instruct",
     ],
 )
 @pytest.mark.neuron_parallel_compile
