28037926dbb698109be1b4f91e6564715669e6b2
Alvaro Moran <alvaro.moran@huggingface.co>
2025-06-23 15:37:58 +0000

    feat(inference): add GPT OSS to AutoModels

--- optimum/neuron/models/inference/auto_models.py
+++ optimum/neuron/models/inference/auto_models.py
@@ -22,6 +22,7 @@ automatically by the neuron factory classes such as NeuronModelForCausalLM.
 import os
 
 from ..auto_model import register_neuron_model
+from .gpt_oss.modeling_gpt_oss import GptOssNxdForCausalLM
 from .granite.modeling_granite import GraniteNxDModelForCausalLM
 from .llama.modeling_llama import LlamaNxDModelForCausalLM
 from .mixtral.modeling_mixtral import MixtralNxDModelForCausalLM
@@ -89,6 +90,15 @@ class MixtralNeuronModelForCausalLM(MixtralNxDModelForCausalLM):
 class Qwen3NeuronModelForCausalLM(Qwen3NxDModelForCausalLM):
     """
     Qwen3 model with NxD backend for inference on AWS Neuron.
+    """
+
+    pass
+
+
+@register_neuron_model_for_inference("gpt_oss", "text-generation")
+class GptOssForCausalLM(GptOssNxdForCausalLM):
+    """
+    GptOss model with NxD backend for inference on AWS Neuron.
     """
 
     pass
