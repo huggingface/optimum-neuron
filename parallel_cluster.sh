dd4465711216d297bc3756654c5f6449f52e2ed6
Michael Benayoun <mickbenayoun@gmail.com>
2025-07-07 18:02:24 +0200

    chore: untrack files that should not be tracked

--- parallel_cluster.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-HOSTNAME=${1:-ec2-34-237-66-4.compute-1.amazonaws.com}
-echo "Connecting to $HOSTNAME"
-ssh -i /Users/michaelbenayoun/Documents/Cles/michael-keypair.pem -o StrictHostKeyChecking=no  ubuntu@$HOSTNAME
--- push_to_hub.sh
+++
@@ -1,1 +0,0 @@
-python synchronize.py --cache_dir ~/cache_dir_neuron/ --repo_id michaelbenayoun/optimum-neuron-cache
