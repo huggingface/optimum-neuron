ca5381e4fe92527d4703a34a2adb9428756cfb61
Michael Benayoun <mickbenayoun@gmail.com>
2025-07-07 12:42:10 +0200

    clean: remove availability checks from granite modeling

--- optimum/neuron/models/training/granite/modeling_granite.py
+++ optimum/neuron/models/training/granite/modeling_granite.py
@@ -24,7 +24,6 @@ from transformers.models.granite.configuration_granite import GraniteConfig
 from transformers.processing_utils import Unpack
 from transformers.utils import LossKwargs, can_return_tuple, logging
 
-from ....utils import is_neuronx_distributed_available, is_torch_xla_available
 from ..config import TrainingNeuronConfig
 from ..llama.modeling_llama import (
     LlamaAttention,
@@ -37,18 +36,16 @@ from ..llama.modeling_llama import (
 )
 
 
-if is_torch_xla_available():
-    from torch_xla.utils.checkpoint import checkpoint
+from torch_xla.utils.checkpoint import checkpoint
 
-if is_neuronx_distributed_available():
-    from neuronx_distributed.parallel_layers.mappings import (
-        gather_from_sequence_parallel_region,
-        scatter_to_sequence_parallel_region,
-    )
+from neuronx_distributed.parallel_layers.mappings import (
+    gather_from_sequence_parallel_region,
+    scatter_to_sequence_parallel_region,
+)
 
-    # Wrap the gather and scatter functions to ensure they are properly traced by `torch.fx`.
-    gather_from_sequence_parallel_region = torch.fx.wrap(gather_from_sequence_parallel_region)
-    scatter_to_sequence_parallel_region = torch.fx.wrap(scatter_to_sequence_parallel_region)
+# Wrap the gather and scatter functions to ensure they are properly traced by `torch.fx`.
+gather_from_sequence_parallel_region = torch.fx.wrap(gather_from_sequence_parallel_region)
+scatter_to_sequence_parallel_region = torch.fx.wrap(scatter_to_sequence_parallel_region)
 
 logger = logging.get_logger(__name__)
 
