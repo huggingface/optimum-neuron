0ff04255c4e9f2e38e29d579fa4b908f61169fb8
Michael Benayoun <mickbenayoun@gmail.com>
2025-07-10 15:07:13 +0200

    example(qwen3): update finetuning bash script

--- examples/training/qwen3/finetune_qwen3.sh
+++ examples/training/qwen3/finetune_qwen3.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
+
+# Flags for Neuron compilation
+export NEURON_CC_FLAGS="--model-type transformer --cache_dir=$HOME/neuron_compile_cache/ --retry_failed_compilation"
+export NEURON_FUSE_SOFTMAX=1
+export NEURON_RT_ASYNC_EXEC_MAX_INFLIGHT_REQUESTS=3 # Async Runtime
+export MALLOC_ARENA_MAX=64 # Host OOM mitigation
+
+# Variables for training
 PROCESSES_PER_NODE=32
 NUM_EPOCHS=3
 TP_DEGREE=8
 BS=1
 GRADIENT_ACCUMULATION_STEPS=8
 LOGGING_STEPS=2
-MODEL_NAME="Qwen/Qwen3-8B"
+MODEL_NAME="Qwen/Qwen3-8B" # Change this to the desired model name
 OUTPUT_DIR="$(echo $MODEL_NAME | cut -d'/' -f2)-finetuned"
 MAX_STEPS=-1
 DISTRIBUTED_ARGS="--nproc_per_node $PROCESSES_PER_NODE"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 torchrun --nproc_per_node $PROCESSES_PER_NODE finetune_qwen3.py \
   --model_id $MODEL_NAME \
