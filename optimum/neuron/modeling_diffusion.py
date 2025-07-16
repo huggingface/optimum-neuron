d8a46b817bec0b7879b1befb20ca3fb3dc415f2c
JingyaHuang <huang_jingya@outlook.com>
2025-07-16 21:52:08 +0000

    fix: move pixart specific

--- optimum/neuron/modeling_diffusion.py
+++ optimum/neuron/modeling_diffusion.py
@@ -1091,7 +1091,10 @@ class NeuronDiffusionPipelineBase(NeuronTracedModel):
         if kwargs.get("image", None):
             kwargs["image"] = self.image_processor.preprocess(kwargs["image"], height=height, width=width)
         # Override default `max_sequence_length`, eg. pixart
-        if "max_sequence_length" in inspect.signature(self.auto_model_class.__call__).parameters:
+        if (
+            "PixArt" in self.auto_model_class.__name__
+            and "max_sequence_length" in inspect.signature(self.auto_model_class.__call__).parameters
+        ):
             encoder = self.text_encoder_2 or self.text_encoder
             kwargs["max_sequence_length"] = encoder.config.neuron.get("static_sequence_length", None)
         return self.auto_model_class.__call__(self, height=height, width=width, *args, **kwargs)
