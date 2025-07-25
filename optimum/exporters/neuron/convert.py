c33666ade2271e0085b82f21d2bd601e7f8db644
JingyaHuang <huang_jingya@outlook.com>
2025-07-25 14:26:30 +0000

    chore: remove comments

--- optimum/exporters/neuron/convert.py
+++ optimum/exporters/neuron/convert.py
@@ -350,11 +350,6 @@ def export_models(
     failed_models = []
     total_compilation_time = 0
     compile_configs = {}
-    # models_and_neuron_configs.pop("text_encoder_2")
-    # models_and_neuron_configs.pop("text_encoder")
-    # models_and_neuron_configs.pop("transformer")
-    # models_and_neuron_configs.pop("vae_encoder")
-    # models_and_neuron_configs.pop("vae_decoder")
     for i, model_name in enumerate(models_and_neuron_configs.keys()):
         logger.info(f"***** Compiling {model_name} *****")
         submodel, sub_neuron_config = models_and_neuron_configs[model_name]
