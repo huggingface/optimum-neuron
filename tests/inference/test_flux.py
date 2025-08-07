78ccb95b0f923a7d38f806200e67c608705d497d
JingyaHuang <huang_jingya@outlook.com>
2025-08-07 11:19:31 +0000

    fix: put back other tests

--- tests/inference/test_flux.py
+++ tests/inference/test_flux.py
@@ -15,9 +15,10 @@
 
 import PIL
 import pytest
+import torch
 from diffusers.utils import load_image
 
-from optimum.neuron import NeuronFluxKontextPipeline
+from optimum.neuron import NeuronFluxInpaintPipeline, NeuronFluxKontextPipeline, NeuronFluxPipeline
 from optimum.neuron.modeling_diffusion import (
     NeuronModelTextEncoder,
     NeuronModelTransformer,
@@ -28,46 +29,46 @@ from optimum.neuron.utils.testing_utils import is_inferentia_test, requires_neur
 from optimum.utils.testing_utils import require_diffusers
 
 
-# @pytest.mark.order(0)
-# @is_inferentia_test
-# @requires_neuronx
-# @require_diffusers
-# def test_flux_txt2img(neuron_flux_tp2_path):
-#     neuron_pipeline = NeuronFluxPipeline.from_pretrained(neuron_flux_tp2_path)
+@pytest.mark.order(0)
+@is_inferentia_test
+@requires_neuronx
+@require_diffusers
+def test_flux_txt2img(neuron_flux_tp2_path):
+    neuron_pipeline = NeuronFluxPipeline.from_pretrained(neuron_flux_tp2_path)
 
-#     assert isinstance(neuron_pipeline.text_encoder, NeuronModelTextEncoder)
-#     assert isinstance(neuron_pipeline.text_encoder_2, NeuronModelTextEncoder)
-#     assert isinstance(neuron_pipeline.transformer, NeuronModelTransformer)
-#     assert isinstance(neuron_pipeline.vae_encoder, NeuronModelVaeEncoder)
-#     assert isinstance(neuron_pipeline.vae_decoder, NeuronModelVaeDecoder)
+    assert isinstance(neuron_pipeline.text_encoder, NeuronModelTextEncoder)
+    assert isinstance(neuron_pipeline.text_encoder_2, NeuronModelTextEncoder)
+    assert isinstance(neuron_pipeline.transformer, NeuronModelTransformer)
+    assert isinstance(neuron_pipeline.vae_encoder, NeuronModelVaeEncoder)
+    assert isinstance(neuron_pipeline.vae_decoder, NeuronModelVaeDecoder)
 
-#     prompt = "Mario eating hamburgers."
-#     image = neuron_pipeline(
-#         prompt, num_inference_steps=4, max_sequence_length=256, generator=torch.Generator("cpu").manual_seed(0)
-#     ).images[0]
-#     assert isinstance(image, PIL.Image.Image)
+    prompt = "Mario eating hamburgers."
+    image = neuron_pipeline(
+        prompt, num_inference_steps=4, max_sequence_length=256, generator=torch.Generator("cpu").manual_seed(0)
+    ).images[0]
+    assert isinstance(image, PIL.Image.Image)
 
 
-# @pytest.mark.order(0)
-# @is_inferentia_test
-# @requires_neuronx
-# @require_diffusers
-# def test_flux_inpaint(neuron_flux_tp2_path):
-#     neuron_pipeline = NeuronFluxInpaintPipeline.from_pretrained(neuron_flux_tp2_path)
+@pytest.mark.order(0)
+@is_inferentia_test
+@requires_neuronx
+@require_diffusers
+def test_flux_inpaint(neuron_flux_tp2_path):
+    neuron_pipeline = NeuronFluxInpaintPipeline.from_pretrained(neuron_flux_tp2_path)
 
-#     assert isinstance(neuron_pipeline.text_encoder, NeuronModelTextEncoder)
-#     assert isinstance(neuron_pipeline.text_encoder_2, NeuronModelTextEncoder)
-#     assert isinstance(neuron_pipeline.transformer, NeuronModelTransformer)
-#     assert isinstance(neuron_pipeline.vae_encoder, NeuronModelVaeEncoder)
-#     assert isinstance(neuron_pipeline.vae_decoder, NeuronModelVaeDecoder)
+    assert isinstance(neuron_pipeline.text_encoder, NeuronModelTextEncoder)
+    assert isinstance(neuron_pipeline.text_encoder_2, NeuronModelTextEncoder)
+    assert isinstance(neuron_pipeline.transformer, NeuronModelTransformer)
+    assert isinstance(neuron_pipeline.vae_encoder, NeuronModelVaeEncoder)
+    assert isinstance(neuron_pipeline.vae_decoder, NeuronModelVaeDecoder)
 
-#     prompt = "Face of a yellow cat, high resolution, sitting on a park bench"
-#     img_url = "https://raw.githubusercontent.com/CompVis/latent-diffusion/main/data/inpainting_examples/overture-creations-5sI6fQgYIuo.png"
-#     mask_url = "https://raw.githubusercontent.com/CompVis/latent-diffusion/main/data/inpainting_examples/overture-creations-5sI6fQgYIuo_mask.png"
-#     source = load_image(img_url)
-#     mask = load_image(mask_url)
-#     image = neuron_pipeline(prompt=prompt, image=source, mask_image=mask, max_sequence_length=256).images[0]
-#     assert isinstance(image, PIL.Image.Image)
+    prompt = "Face of a yellow cat, high resolution, sitting on a park bench"
+    img_url = "https://raw.githubusercontent.com/CompVis/latent-diffusion/main/data/inpainting_examples/overture-creations-5sI6fQgYIuo.png"
+    mask_url = "https://raw.githubusercontent.com/CompVis/latent-diffusion/main/data/inpainting_examples/overture-creations-5sI6fQgYIuo_mask.png"
+    source = load_image(img_url)
+    mask = load_image(mask_url)
+    image = neuron_pipeline(prompt=prompt, image=source, mask_image=mask, max_sequence_length=256).images[0]
+    assert isinstance(image, PIL.Image.Image)
 
 
 @pytest.mark.order(0)
