995fbcbe878fb224b029ae786907e8aa82e06a1b
Alvaro Moran <alvaro.moran@huggingface.co>
2025-08-27 13:56:20 +0000

    test(gpt oss): validate modules

--- tests/decoder/test_gpt_oss.py
+++ tests/decoder/test_gpt_oss.py
@@ -138,6 +138,7 @@ def neuron_oai_decoder_path(neuron_decoder_oai_config):
 
 @pytest.fixture(scope="module")
 def model_and_tokenizer(neuron_oai_decoder_path):
+    breakpoint()
     model = NeuronModelForCausalLM.from_pretrained(neuron_oai_decoder_path)
     tokenizer = AutoTokenizer.from_pretrained(neuron_oai_decoder_path)
     yield (model, tokenizer)
@@ -155,3 +156,132 @@ def test_gpt_oss(model_and_tokenizer: tuple[NeuronModelForCausalLM, AutoTokenize
     # breakpoint()
     # logits = model.forward(**inputs, return_logits=True)
     # print(logits)
+
+
+import torch
+from transformers import AutoModelForCausalLM, set_seed
+from neuronx_distributed_inference.utils.testing import build_module, validate_accuracy
+from transformers.models.gpt_oss.modeling_gpt_oss import (
+    GptOssForCausalLM, GptOssRMSNorm, GptOssExperts, GptOssTopKRouter
+)
+from optimum.neuron.models.inference.gpt_oss.modeling_gpt_oss import CustomRMSNorm, _experts_swiglu_activation
+from optimum.neuron.models.inference.backend.modules.moe.expert_mlps import ExpertMLPs
+
+@is_inferentia_test
+@requires_neuronx
+def _test_gpt_vs_cpu(model_and_tokenizer: tuple[NeuronModelForCausalLM, AutoTokenizer]):
+    prompt = "Gravity is"
+    checkpoint = "tengomucho/tiny-random-gpt-oss"
+    device = "cpu"
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+    model_cpu = GptOssForCausalLM.from_pretrained(checkpoint).to(device)
+    inputs = tokenizer.encode(prompt, return_tensors="pt").to(device)
+    max_new_tokens = 10
+    outputs = model_cpu.generate(inputs, max_new_tokens=max_new_tokens, do_sample=False)
+    generated_text = tokenizer.decode(outputs[0])
+    print(generated_text)
+
+    model, tokenizer = model_and_tokenizer
+    inputs = tokenizer.encode(prompt, return_tensors="pt")
+    outputs = model.generate(inputs, max_new_tokens=max_new_tokens, do_sample=False)
+    generated_text = tokenizer.decode(outputs[0])
+    print(generated_text)
+    # Comparison would fail
+
+
+@is_inferentia_test
+@requires_neuronx
+def test_gpt_oss_rms_norm():
+    checkpoint = "tengomucho/tiny-random-gpt-oss"
+    config = GptOssConfig.from_pretrained(checkpoint)
+    hidden_size = config.hidden_size
+    seq_len = 2
+    hidden_states = torch.ones(1, seq_len, hidden_size, dtype=torch.bfloat16)
+    inputs = [(hidden_states,)]
+
+    example_inputs = [tuple([torch.zeros_like(input_element) for input_element in input_elements]) for input_elements in inputs]
+
+    # Create a cpu layer
+    cpu_module = GptOssRMSNorm(hidden_size)
+
+    # Create a function to run the model
+    neuron_module = build_module(CustomRMSNorm, example_inputs, module_init_kwargs={"hidden_size": hidden_size})
+
+    # Validate the accuracy of the model
+    validate_accuracy(neuron_module, inputs, cpu_callable=cpu_module)
+
+
+def _set_weights(module):
+    """Set the weights of the module to random values"""
+    state_dict = module.state_dict()
+    for key in list(state_dict.keys()):
+        state_dict[key] = torch.nn.Parameter(torch.rand_like(state_dict[key]) * 0.1)
+    module.load_state_dict(state_dict)
+
+
+@is_inferentia_test
+@requires_neuronx
+@torch.no_grad()
+def test_gpt_oss_experts():
+    set_seed(123)
+    checkpoint = "tengomucho/tiny-random-gpt-oss"
+    config = GptOssConfig.from_pretrained(checkpoint)
+
+    # Prepare the input
+    hidden_size = config.hidden_size
+    seq_len = 2
+    dtype = torch.float32
+    # Make hidden_states between -3 and 3
+    hidden_states = torch.rand(seq_len, hidden_size, dtype=dtype).sin() * 7 - 3
+    # get routing weights and indices by using a router
+    router = GptOssTopKRouter(config)
+    _set_weights(router)
+    routing_weights, indices = router(hidden_states)
+
+    inputs = [hidden_states, routing_weights, indices, torch.tensor(seq_len)]
+    example_inputs = [torch.zeros_like(input_element, dtype=input_element.dtype) for input_element in inputs]
+    inputs = [tuple(inputs)]
+    example_inputs = [tuple(example_inputs)]
+
+    # Create a cpu layer
+    cpu_module = GptOssExperts(config)
+    _set_weights(cpu_module)
+
+    def wrapped_cpu_module(hidden_states, routing_weights, indices, seq_len):
+        hidden_states = hidden_states.reshape(1, seq_len, hidden_size)
+        ret = cpu_module(hidden_states, indices, routing_weights)
+        return ret.reshape(seq_len, hidden_size)
+
+    # Create a function to run the model
+    neuron_model = build_module(ExpertMLPs, example_inputs, module_init_kwargs={
+        "num_experts": config.num_local_experts,
+        "top_k": config.num_experts_per_tok,
+        "hidden_size": hidden_size,
+        "intermediate_size": config.intermediate_size,
+        "hidden_act": config.hidden_act,
+        "glu_mlp": True,
+        "capacity_factor": 1.0,
+        "expert_bias": True,
+        "normalize_top_k_affinities": True,
+        "glu_activation_fn": _experts_swiglu_activation
+    })
+
+    state_dict = cpu_module.state_dict()
+    # patch state_dict to prepend keys with "mlp_op"
+    for key in list(state_dict.keys()):
+        new_key = f"mlp_op.{key}"
+        if new_key.endswith("_bias"):
+            new_key = new_key.replace("_bias", ".bias")
+        else:
+            new_key += ".weight"
+        state_dict[new_key] = state_dict.pop(key)
+
+    # neuron_module.load_state_dict(state_dict)
+    start_rank_id = 0
+    start_rank_tensor = torch.tensor([start_rank_id], dtype=torch.int32, device="cpu")
+    weights = [state_dict]
+    neuron_model.nxd_model.initialize(weights, start_rank_tensor)
+
+    # Validate the accuracy of the model
+    validate_accuracy(neuron_model, inputs, cpu_callable=wrapped_cpu_module)
