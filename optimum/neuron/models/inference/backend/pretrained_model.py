570c6aceb7a9d78085aead2a8c142cd4cd4d914f
David Corvoysier <david@huggingface.co>
2025-08-29 13:24:16 +0000

    refactor(nxd): remove neff_cache context
    Patching the generate_neff method to instantiate and use a libneuronxla Cache
    object to check for cached neffs is not required anymore, since the ModelBuilder
    now does it also.
    Note that we still need to use the hub_neuronx_cache context to patch the
    libneuronxla create_compile_cache method and use the hf hub cache proxy instead
    of the local cache.

--- optimum/neuron/models/inference/backend/pretrained_model.py
+++ optimum/neuron/models/inference/backend/pretrained_model.py
@@ -25,7 +25,6 @@ from neuronx_distributed.trace.model_builder import ModelBuilder
 from safetensors.torch import load_file
 from transformers import AutoModelForCausalLM, PretrainedConfig
 
-from .cache import neff_cache
 from .config import NxDNeuronConfig
 from .model_wrapper import NxDModelWrapper
 from .modules.checkpoint import (
@@ -149,8 +148,7 @@ class NxDPreTrainedModel:
     @staticmethod
     def compile(neuron_config, model_wrappers: list[NxDModelWrapper], compiler_args: str, debug: bool = False):
         builder = get_builder(neuron_config, model_wrappers, debug=debug, compiler_args=compiler_args)
-        with neff_cache():
-            return builder.trace(initialize_model_weights=False)
+        return builder.trace(initialize_model_weights=False)
 
     def save(self, dest_path, weight_path: str | None = None):
         if self._traced_model is None:
