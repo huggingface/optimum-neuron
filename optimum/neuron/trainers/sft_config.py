73aa8e79069ee0113bdf74833eec5d559bb306ca
Michael Benayoun <mickbenayoun@gmail.com>
2025-09-03 16:13:10 +0200

    refactor: add sft_config.py file instead of having it in trl_utils.py

---
+++ optimum/neuron/trainers/sft_config.py
@@ -0,0 +1,35 @@
+# coding=utf-8
+# Copyright 2025 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# Seg the License for the specific language governing permissions and
+# limitations under the License.
+
+from dataclasses import dataclass
+
+from ..utils.import_utils import is_trl_available
+from .training_args import NeuronTrainingArguments
+from .trl_utils import TRL_VERSION
+
+
+if is_trl_available():
+    from trl import SFTConfig
+else:
+
+    @dataclass
+    class SFTConfig:
+        def __init__(self, *args, **kwargs):
+            raise RuntimeError(f"You need to install the `trl=={TRL_VERSION}` library to use the `NeuronSFTConfig`.")
+
+
+@dataclass
+class NeuronSFTConfig(NeuronTrainingArguments, SFTConfig):
+    pass
