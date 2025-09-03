3d97532b40a7df3a836eb3558fb981123754e50e
David Corvoysier <david@huggingface.co>
2025-09-03 13:06:06 +0000

    refactor(inference): rename CustomRMSNorm

--- optimum/neuron/models/inference/backend/modules/rms_norm.py
+++ optimum/neuron/models/inference/backend/modules/rms_norm.py
@@ -18,11 +18,11 @@ from torch import nn, ones
 from torch_neuronx.xla_impl.ops import RmsNorm
 
 
-class CustomRMSNorm(nn.Module):
+class NeuronRMSNorm(nn.Module):
     def __init__(self, hidden_size, eps=1e-6):
         """
         Use this RMSNorm to perform customized rmsnorm on Neuron
-        Note: CustomRMSNorm forward method calls target="AwsNeuronRmsNorm"
+        Note: NeuronRMSNorm forward method calls target="AwsNeuronRmsNorm"
         """
         super().__init__()
         self.weight = nn.Parameter(ones(hidden_size))
