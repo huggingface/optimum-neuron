3603b2ee0ad8df9dc10cba7c8226d0457293d836
Michael Benayoun <mickbenayoun@gmail.com>
2025-09-05 16:56:04 +0200

    fix: typo in sft_trainer docstring

--- optimum/neuron/trainers/sft_trainer.py
+++ optimum/neuron/trainers/sft_trainer.py
@@ -61,9 +61,9 @@ else:
         pass
 
 
-# Create a new class that inherints from NeuronTrainer to use this class instead of the transformers Trainer,
+# Create a new class that inherits from NeuronTrainer to use this class instead of the transformers Trainer,
 # but has the same methods and attributes as SFTTrainer.
-# We can then inherint from this class to create our NeuronSFTTrainer.
+# We can then inherit from this class to create our NeuronSFTTrainer.
 _SFTTrainer = type(
     "_SFTTrainer",
     (NeuronTrainer,),
