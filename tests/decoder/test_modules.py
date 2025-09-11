c4f5b87494570c902c1d8c287ca3e44efaea492b
Alvaro Moran <alvaro.moran@huggingface.co>
2025-09-11 14:25:39 +0000

    test(inference): add tests to check decoder layer accuracy

--- tests/decoder/test_modules.py
+++ tests/decoder/test_modules.py
@@ -1,12 +1,31 @@
+import os
+from dataclasses import dataclass
+from tempfile import TemporaryDirectory
+from typing import Callable
+
+import pytest
 import torch
 from nxd_testing import build_module, validate_accuracy
 from transformers import AutoConfig, set_seed
-from transformers.models.llama.modeling_llama import LlamaMLP, LlamaRMSNorm, LlamaRotaryEmbedding
+from transformers.models.llama.modeling_llama import (
+    LlamaDecoderLayer,
+    LlamaMLP,
+    LlamaRMSNorm,
+    LlamaRotaryEmbedding,
+)
+from transformers.models.mixtral.modeling_mixtral import (
+    MixtralDecoderLayer,
+    MixtralRotaryEmbedding,
+)
 
 from optimum.neuron.models.inference.backend.config import NxDNeuronConfig
 from optimum.neuron.models.inference.backend.modules.rms_norm import NeuronRMSNorm
 from optimum.neuron.models.inference.llama.modeling_llama import Llama3RotaryEmbedding as NeuronLlama3RotaryEmbedding
-from optimum.neuron.models.inference.llama.modeling_llama import NeuronLlamaMLP
+from optimum.neuron.models.inference.llama.modeling_llama import NeuronLlamaDecoderLayer, NeuronLlamaMLP
+from optimum.neuron.models.inference.mixtral.modeling_mixtral import (
+    NeuronMixtralDecoderLayer,
+    convert_mixtral_to_neuron_state_dict,
+)
 from optimum.neuron.utils.testing_utils import is_inferentia_test, requires_neuronx
 
 
@@ -137,6 +156,141 @@ def test_llama_rotary_embedding():
 
     # Validate the accuracy of the model. Note that atol and rtol are relaxed considering calculations in the neuron
     # model are in bfloat16 and errors accumulate.
+    validate_accuracy(
+        neuron_module,
+        inputs,
+        cpu_callable=cpu_module_wrapper,
+        assert_close_kwargs={"atol": torch.finfo(torch.bfloat16).resolution, "rtol": 1e-1},
+    )
+
+
+def _convert_state_dict_for_mixtral(state_dict, config, neuron_config):
+    # Add "layers.0" prefix to the state dict keys to use "convert_mixtral_to_neuron_state_dict"
+    keys = list(state_dict.keys())
+    for key in keys:
+        state_dict[f"layers.0.{key}"] = state_dict.pop(key)
+    state_dict = convert_mixtral_to_neuron_state_dict(state_dict, config, neuron_config)
+    # Remove "layers.0" prefix from the state dict keys
+    keys = list(state_dict.keys())
+    for key in keys:
+        state_dict[key.replace("layers.0.", "")] = state_dict.pop(key)
+    return state_dict
+
+
+@dataclass
+class DecoderLayerTestConfig:
+    name: str
+    config_id: str
+    decoder_layer_cls: torch.nn.Module
+    neuron_decoder_layer_cls: torch.nn.Module
+    rotary_embedding_cls: torch.nn.Module
+    neuron_init_kwargs: Callable[[AutoConfig], dict]
+    state_dict_conversion_fn: Callable[[dict, AutoConfig, NxDNeuronConfig], dict]
+
+
+DECODER_TESTS_CONFIGS = [
+    DecoderLayerTestConfig(
+        name="llama",
+        config_id="unsloth/Llama-3.2-1B-Instruct",
+        decoder_layer_cls=LlamaDecoderLayer,
+        neuron_decoder_layer_cls=NeuronLlamaDecoderLayer,
+        rotary_embedding_cls=LlamaRotaryEmbedding,
+        neuron_init_kwargs=lambda config, neuron_config: {"config": config, "neuron_config": neuron_config},
+        state_dict_conversion_fn=lambda state_dict, config, neuron_config: state_dict,
+    ),
+    DecoderLayerTestConfig(
+        name="mixtral",
+        config_id="dacorvo/Mixtral-tiny",
+        decoder_layer_cls=MixtralDecoderLayer,
+        neuron_decoder_layer_cls=NeuronMixtralDecoderLayer,
+        rotary_embedding_cls=MixtralRotaryEmbedding,
+        neuron_init_kwargs=lambda config, neuron_config: {
+            "config": config,
+            "neuron_config": neuron_config,
+            "layer_idx": 0,
+        },
+        state_dict_conversion_fn=_convert_state_dict_for_mixtral,
+    ),
+]
+
+
+@is_inferentia_test
+@requires_neuronx
+@pytest.mark.parametrize(
+    "test_config", DECODER_TESTS_CONFIGS, ids=[test_config.name for test_config in DECODER_TESTS_CONFIGS]
+)
+def test_decoder_layer(test_config: DecoderLayerTestConfig):
+    set_seed(42)
+    config_id = test_config.config_id
+    config = AutoConfig.from_pretrained(config_id)
+    hidden_size = config.hidden_size
+    dtype = torch.float32
+    seq_len = 128
+    # Initialize hidden states to random values between -3 and 3
+    hidden_states = torch.rand(1, seq_len, hidden_size, dtype=dtype) * 6 - 3
+    position_ids = torch.arange(seq_len, dtype=torch.int64).view(1, -1)
+    attention_mask = torch.ones(1, 1, seq_len, seq_len).to(torch.bool).tril(diagonal=0)
+    inputs = [(hidden_states, attention_mask, position_ids)]
+    example_inputs = [
+        tuple([torch.zeros_like(input_element, dtype=input_element.dtype) for input_element in input_elements])
+        for input_elements in inputs
+    ]
+
+    example_inputs = [
+        tuple([torch.zeros_like(input_element) for input_element in input_elements]) for input_elements in inputs
+    ]
+
+    config.num_hidden_layers = 1  # This is set to 1 to match the number of decoder layers tested here.
+    config._attn_implementation = "eager"  # Force eager attention in cpu
+    cpu_module = test_config.decoder_layer_cls(config, 0)
+    _set_weights(cpu_module)
+
+    rotary_embedding = test_config.rotary_embedding_cls(config)
+
+    def cpu_module_wrapper(hidden_states, attention_mask, position_ids):
+        # Position embeddings are computed outside the layer in transformers
+        position_embeddings = rotary_embedding(hidden_states, position_ids)
+        # CPU version requires the mask to be in float32
+        attention_mask_cpu = torch.where(attention_mask, 0.0, torch.finfo(torch.float32).min)
+
+        return cpu_module(
+            hidden_states,
+            attention_mask=attention_mask_cpu,
+            position_ids=position_ids,
+            use_cache=False,
+            position_embeddings=position_embeddings,
+        )
+
+    # Make a wrapper to consider only hidden states and ignore other inputs
+    class DecoderLayerWrapper(test_config.neuron_decoder_layer_cls):
+        def forward(self, hidden_states, attention_mask, position_ids):
+            hidden_states, _present_key_value, _cos_cache, _sin_cache = super().forward(
+                hidden_states, attention_mask, position_ids
+            )
+            return hidden_states
+
+    # Create a function to run the model
+    neuron_config = NxDNeuronConfig(
+        batch_size=1,
+        sequence_length=seq_len,
+    )
+    state_dict = cpu_module.state_dict()
+    state_dict = test_config.state_dict_conversion_fn(state_dict, config, neuron_config)
+
+    with TemporaryDirectory() as tmpdir:
+        # There are many quirks in the neuron attention implementation, so we will just save the state dict and load it
+        # again to build the module.
+        checkpoint_path = os.path.join(tmpdir, "checkpoint.pt")
+        torch.save(state_dict, checkpoint_path)
+
+        neuron_module = build_module(
+            DecoderLayerWrapper,
+            example_inputs,
+            module_init_kwargs=test_config.neuron_init_kwargs(config, neuron_config),
+            checkpoint_path=checkpoint_path,
+        )
+
+    # Validate the accuracy of the model
     validate_accuracy(
         neuron_module,
         inputs,
