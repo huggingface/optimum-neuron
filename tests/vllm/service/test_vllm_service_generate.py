fac0795c0905c9aac1d5b82b363131dcd51b586d
David Corvoysier <david@huggingface.co>
2025-09-26 11:27:49 +0000

    review: regroup test fixtures

--- tests/vllm/service/test_vllm_service_generate.py
+++ tests/vllm/service/test_vllm_service_generate.py
@@ -12,85 +12,47 @@ async def multi_model_vllm_service(vllm_launcher, neuron_llm_config):
         yield vllm_service
 
 
-@pytest.fixture
-async def vllm_service_from_local_neuron_model(vllm_launcher, base_neuron_llm_config):
-    model_name_or_path = base_neuron_llm_config["neuron_model_path"]
+@pytest.mark.asyncio
+@pytest.fixture(params=["local_neuron", "hub_neuron", "hub"])
+async def vllm_service_from_model(request, vllm_launcher, base_neuron_llm_config):
     service_name = base_neuron_llm_config["name"]
-    with vllm_launcher(service_name, model_name_or_path) as vllm_service:
-        await vllm_service.health(600)
-        yield vllm_service
+    if request.param == "hub":
+        model_name_or_path = base_neuron_llm_config["model_id"]
+        export_kwargs = base_neuron_llm_config["export_kwargs"]
+        batch_size = export_kwargs["batch_size"]
+        sequence_length = export_kwargs["sequence_length"]
+        tensor_parallel_size = export_kwargs["tensor_parallel_size"]
+        dtype = DTYPE_MAPPER.pt(export_kwargs["auto_cast_type"])
+        with vllm_launcher(
+            service_name,
+            model_name_or_path,
+            batch_size=batch_size,
+            sequence_length=sequence_length,
+            tensor_parallel_size=tensor_parallel_size,
+            dtype=dtype,
+        ) as vllm_service:
+            await vllm_service.health(600)
+            yield vllm_service
+    else:
+        if request.param == "local_neuron":
+            model_name_or_path = base_neuron_llm_config["neuron_model_path"]
+        else:
+            model_name_or_path = base_neuron_llm_config["neuron_model_id"]
+        with vllm_launcher(service_name, model_name_or_path) as vllm_service:
+            await vllm_service.health(600)
+            yield vllm_service
 
 
-@pytest.fixture
-async def vllm_service_from_hub_neuron_model(vllm_launcher, base_neuron_llm_config):
-    model_name_or_path = base_neuron_llm_config["neuron_model_id"]
-    service_name = base_neuron_llm_config["name"]
-    with vllm_launcher(service_name, model_name_or_path) as vllm_service:
-        await vllm_service.health(600)
-        yield vllm_service
-
-
-@pytest.fixture
-async def vllm_service_from_hub_model(vllm_launcher, base_neuron_llm_config):
-    model_name_or_path = base_neuron_llm_config["model_id"]
-    service_name = base_neuron_llm_config["name"]
-    export_kwargs = base_neuron_llm_config["export_kwargs"]
-    batch_size = export_kwargs["batch_size"]
-    sequence_length = export_kwargs["sequence_length"]
-    tensor_parallel_size = export_kwargs["tensor_parallel_size"]
-    dtype = DTYPE_MAPPER.pt(export_kwargs["auto_cast_type"])
-    with vllm_launcher(
-        service_name,
-        model_name_or_path,
-        batch_size=batch_size,
-        sequence_length=sequence_length,
-        tensor_parallel_size=tensor_parallel_size,
-        dtype=dtype,
-    ) as vllm_service:
-        await vllm_service.health(600)
-        yield vllm_service
-
-
-async def _test_vllm_service_base(vllm_service, prompt, max_output_tokens):
-    greedy_tokens, greedy_text = await vllm_service.client.greedy(prompt, max_output_tokens=max_output_tokens)
-
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "prompt, max_output_tokens", [("One of my fondest memory is", 32), ("What is the colour of the sky ?", 24)]
+)
+async def test_vllm_service_from_model(vllm_service_from_model, prompt, max_output_tokens):
+    greedy_tokens, greedy_text = await vllm_service_from_model.client.greedy(
+        prompt, max_output_tokens=max_output_tokens
+    )
+    print(f"Greedy output: {greedy_text}")
     assert greedy_tokens == max_output_tokens
-
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize(
-    "prompt, max_output_tokens",
-    [
-        ("What is Deep Learning?", 17),
-        ("One of my fondest memory is", 32),
-    ],
-)
-async def test_vllm_service_from_local_neuron_model(vllm_service_from_local_neuron_model, prompt, max_output_tokens):
-    await _test_vllm_service_base(vllm_service_from_local_neuron_model, prompt, max_output_tokens)
-
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize(
-    "prompt, max_output_tokens",
-    [
-        ("What is Deep Learning?", 17),
-        ("One of my fondest memory is", 32),
-    ],
-)
-async def test_vllm_service_from_hub_neuron_model(vllm_service_from_hub_neuron_model, prompt, max_output_tokens):
-    await _test_vllm_service_base(vllm_service_from_hub_neuron_model, prompt, max_output_tokens)
-
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize(
-    "prompt, max_output_tokens",
-    [
-        ("What is Deep Learning?", 17),
-        ("One of my fondest memory is", 32),
-    ],
-)
-async def test_vllm_service_from_hub_model(vllm_service_from_hub_model, prompt, max_output_tokens):
-    await _test_vllm_service_base(vllm_service_from_hub_model, prompt, max_output_tokens)
 
 
 @pytest.mark.asyncio
