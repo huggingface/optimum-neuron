ee6c3aba8c080149a3e455007bc7d707c95693a3
Michael Benayoun <mickbenayoun@gmail.com>
2025-09-29 14:41:35 +0200

    fix: overfit tests do not work

--- optimum/neuron/utils/training_utils.py
+++ optimum/neuron/utils/training_utils.py
@@ -185,13 +185,6 @@ def is_logging_process() -> bool:
     return dp_rank == tp_rank == 0 and pp_rank == pp_size - 1
 
 
-def is_logging_process_method(self) -> bool:
-    """
-    Method version of `is_logging_process`, useful when this is used to patch a method from the Trainer class.
-    """
-    return is_logging_process()
-
-
 def is_custom_modeling_model(model) -> bool:
     from peft import PeftModel
 
