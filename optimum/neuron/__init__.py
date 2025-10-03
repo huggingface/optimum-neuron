13c1472a7b75ab6cf0c024bdfadab2da1f417efc
David Corvoysier <david@huggingface.co>
2025-10-03 12:43:28 +0000

    feat: add warning when setting defaut target platform

--- optimum/neuron/__init__.py
+++ optimum/neuron/__init__.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 
 import os
+import warnings
 from typing import TYPE_CHECKING
 
 from transformers.utils import _LazyModule
@@ -22,8 +23,11 @@ from .utils.system import get_neuron_major
 
 
 if get_neuron_major() == -1:
-    # It seems this is running on a CPU-only machine, so we override the platform target to trn1, otherwise there will
-    # be errors when importing torch_neuronx.
+    warnings.warn(
+        "It seems this is running on a CPU-only machine,"
+        " so we override the platform target to trn1, otherwise there will"
+        " be errors when importing torch_neuronx."
+    )
     os.environ["NEURON_PLATFORM_TARGET_OVERRIDE"] = "trn1"
 
 
