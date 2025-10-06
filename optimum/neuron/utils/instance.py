8763dfd13b3efcf2538f7af5ad676a8d75dfa96a
David Corvoysier <david@huggingface.co>
2025-10-06 07:49:46 +0000

    feat: add instance type helpers

---
+++ optimum/neuron/utils/instance.py
@@ -0,0 +1,50 @@
+# coding=utf-8
+# Copyright 2025 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import functools
+
+from .system import get_available_cores
+
+
+SUPPORTED_INSTANCE_TYPES = ["trn1", "inf2", "trn1n", "trn2"]
+INSTANCE_VALUE_MAP = {
+    "trn1": "trn1",
+    "trn1n": "trn1",
+    "inf2": "trn1",
+    "trn2": "trn2",
+}
+
+
+@functools.cache
+def current_instance_type() -> str:
+    if get_available_cores() == 0:
+        raise RuntimeError("No Neuron device detected.")
+    fpath = "/sys/devices/virtual/dmi/id/product_name"
+    try:
+        with open(fpath, "r") as f:
+            fc = f.readline()
+    except IOError:
+        raise RuntimeError("Unable to read Neuron platform instance type.")
+    instance_type = fc.split(".")[0]
+    return normalize_instance_type(instance_type)
+
+
+def normalize_instance_type(instance_type: str) -> str:
+    if instance_type not in SUPPORTED_INSTANCE_TYPES:
+        raise ValueError(
+            f"{instance_type} is not a valid instance type, supported instance types are: {SUPPORTED_INSTANCE_TYPES}."
+        )
+
+    # Normalize instance type
+    return INSTANCE_VALUE_MAP[instance_type]
