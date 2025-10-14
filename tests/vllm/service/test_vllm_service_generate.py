0b5b58c638f3446010c32859a3eb8565ae3024d2
David Corvoysier <david@huggingface.co>
2025-10-14 12:17:49 +0000

    test(vllm): do not check expectations in service tests
    Expectations tests are difficult to maintain because they are dependent
    on subtle changes in the execution graph that may happen as the software
    stack evolves.

--- tests/vllm/service/test_vllm_service_generate.py
+++ tests/vllm/service/test_vllm_service_generate.py
@@ -65,23 +65,9 @@ async def test_vllm_service_from_model(vllm_service_from_model, prompt, max_outp
 
 @pytest.mark.asyncio
 async def test_vllm_service_greedy_generation(multi_model_vllm_service):
-    service_name = multi_model_vllm_service.client.service_name
     prompt = "What is Deep Learning?"
     max_output_tokens = 17
     # Greedy bounded without input
-    greedy_tokens, greedy_text = await multi_model_vllm_service.client.greedy(
-        prompt, max_output_tokens=max_output_tokens
-    )
+    greedy_tokens, _ = await multi_model_vllm_service.client.greedy(prompt, max_output_tokens=max_output_tokens)
 
     assert greedy_tokens == max_output_tokens
-
-    greedy_expectations = {
-        "llama": "Deep learning is a subfield of machine learning that involves the use of neural networks with",
-        "qwen2": "Deep Learning is a subset of Machine Learning that involves the use of artificial neural networks to",
-        "granite": "Deep Learning is a subset of machine learning, which is itself a branch of artificial",
-        "qwen3": "<think>\nOkay, the user is asking about what Deep Learning is. Let me start",
-        "phi": " Deep learning is a subset of machine learning, which is itself a subset of artificial intelligence",
-        "smollm3": "<think>\nOkay, so I need to explain what Deep Learning is. Let me start",
-    }
-    # Compare expectations in a case-insensitive way as the results may slightly vary when the enviroment changes
-    assert greedy_text.lower() == greedy_expectations[service_name].lower()
