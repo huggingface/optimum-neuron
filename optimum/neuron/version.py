a3ded34ae936f7b87a011ecc547aaae10af6e93a
Alvaro Moran <alvaro.moran@huggingface.co>
2025-10-23 14:51:34 +0000

    chore: release 0.4.1

--- optimum/neuron/version.py
+++ optimum/neuron/version.py
@@ -12,6 +12,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-__version__ = "0.4.1.dev0"
+__version__ = "0.4.1"
 
 __sdk_version__ = "2.26.0"
