ce301e4721f788619fcea9734d52e5edda99504b
Matthew Zhang <68044987+mzhang4life@users.noreply.github.com>
2025-10-27 13:25:39 -0700

    Neuron grpo config (#2)
    * training_args for grpo
    
    * grpo_config and grpo_trianing_args created
    
    * changed grpo_config
    
    * replaced neuronsftconfig with neuronGRPOConfig
    
    * changed class name
    
    ---------
    
    Co-authored-by: Matthew Zhang <matthew_zhang27@berkeley.edu>

--- optimum/neuron/trainers/grpo_config.py
+++ optimum/neuron/trainers/grpo_config.py
@@ -1,3 +1,27 @@
 # SAAS
 #
 #
+from dataclasses import dataclass
+
+from ..utils.import_utils import is_trl_available
+from .training_args import NeuronTrainingArguments
+from .trl_utils import TRL_VERSION
+
+
+if is_trl_available():
+    from trl import GRPOConfig
+else:
+
+    @dataclass
+    class GRPOConfig:
+        def __init__(self, *args, **kwargs):
+            raise RuntimeError(f"You need to install the `trl=={TRL_VERSION}` library to use the `NeuronGRPOConfig`.")
+
+
+@dataclass
+class NeuronGRPOConfig(NeuronTrainingArguments, GRPOConfig):
+    def __post_init__(self):
+        self.do_eval = False
+        self.eval_strategy = "no"
+        self.eval_steps = None
+        super().__post_init__()
