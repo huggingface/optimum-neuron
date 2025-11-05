72f87ff15be099f328dc03595092bc153fc4f621
JingyaHuang <huang_jingya@outlook.com>
2025-11-05 10:53:58 +0000

    removal: skipped test

--- optimum/neuron/modeling_traced.py
+++ optimum/neuron/modeling_traced.py
@@ -253,11 +253,6 @@ class NeuronTracedModel(OptimizedModel, NeuronModel):
         )
 
     @classmethod
-    def _from_transformers(cls, *args, **kwargs):
-        # Deprecate it when optimum uses `_export` as from_pretrained_method in a stable release.
-        return cls._export(*args, **kwargs)
-
-    @classmethod
     def _export(
         cls,
         model_id: str,
