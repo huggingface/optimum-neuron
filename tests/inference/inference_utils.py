c5e654bb941875094190e39b58f7d22ecdb5fc59
JingyaHuang <huang_jingya@outlook.com>
2025-11-05 10:53:58 +0000

    removal: skipped test

--- tests/inference/inference_utils.py
+++ tests/inference/inference_utils.py
@@ -108,6 +108,7 @@ class NeuronModelIntegrationTestMixin(unittest.TestCase):
 
 class NeuronModelTestMixin(unittest.TestCase):
     ARCH_MODEL_MAP = {}
+    TASK = None
     STATIC_INPUTS_SHAPES = {"batch_size": 1, "sequence_length": 32}
 
     @classmethod
@@ -141,6 +142,7 @@ class NeuronModelTestMixin(unittest.TestCase):
                 model_id,
                 **model_args,
                 export=True,
+                task=self.TASK,
                 torch_dtype=torch.float32,
                 dynamic_batch_size=dynamic_batch_size,
                 **self.STATIC_INPUTS_SHAPES,
