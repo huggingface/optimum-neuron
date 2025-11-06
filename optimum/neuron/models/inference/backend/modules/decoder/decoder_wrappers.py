064c8df08654da79b633720e298ed36efe059cf9
David Corvoysier <david@huggingface.co>
2025-11-06 10:31:12 +0000

    refactor(decoder): simplify padding code

--- optimum/neuron/models/inference/backend/modules/decoder/decoder_wrappers.py
+++ optimum/neuron/models/inference/backend/modules/decoder/decoder_wrappers.py
@@ -52,26 +52,20 @@ class NxDDecoderWrapperForCausalLM(NxDModelWrapper):
 
     def _forward_with_pad(self, input_ids, attention_mask, position_ids, seq_ids, sampling_params):
         # pad the inputs up to the compiled batch size in the end
-        def pad_helper(tensor, pad_type="zeros"):
-            VALID_PAD_TYPES = {"zeros", "ones", "repeat_first_batchline"}
-            assert pad_type in VALID_PAD_TYPES, f"Found {pad_type=}, but valid pad types are {VALID_PAD_TYPES}"
+        def pad_helper(tensor):
             if tensor is None or tensor.shape[0] == self.neuron_config.batch_size:
                 return tensor
 
             padded_shape = list(tensor.shape)
             padded_shape[0] = self.neuron_config.batch_size
-            if pad_type == "repeat_first_batchline":
-                # pad with first batch line values instead of zeros, to reduce chances of NaN
-                padded_tensor = tensor[0].unsqueeze(0).repeat(padded_shape[0], 1).to(tensor.dtype)
-            else:
-                fill_value = 0 if pad_type == "zeros" else 1
-                padded_tensor = torch.full(padded_shape, fill_value=fill_value, dtype=tensor.dtype)
+            # pad with first batch line values instead of zeros, to reduce chances of NaN
+            padded_tensor = tensor[0].unsqueeze(0).repeat(padded_shape[0], 1).to(tensor.dtype)
             padded_tensor[: tensor.shape[0]] = tensor
             return padded_tensor
 
         padded_args = []
         for arg in (input_ids, attention_mask, position_ids):
-            padded_args.append(pad_helper(arg, pad_type="repeat_first_batchline"))
+            padded_args.append(pad_helper(arg))
 
         # need to handle seq_ids separately, when compiled batch is 4, if we pad seq_ids from [0,2,1] to [0,2,1,
         # 0]. then the kv cache of padded input could be written into the first cache line, so we need to pad as [0,
@@ -85,7 +79,7 @@ class NxDDecoderWrapperForCausalLM(NxDModelWrapper):
         padded_args.append(padded_seq_ids)
 
         # pad sampling params by repeating first batchline
-        padded_sampling_params = pad_helper(sampling_params, pad_type="repeat_first_batchline")
+        padded_sampling_params = pad_helper(sampling_params)
         padded_args.append(padded_sampling_params)
 
         outputs = self._forward(*padded_args)
