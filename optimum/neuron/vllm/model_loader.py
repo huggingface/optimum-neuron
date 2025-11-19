b8a0002bdfc2dce5bf2e7059f9c21d894e8991bd
Jim Burtoft <39492751+jimburtoft@users.noreply.github.com>
2025-11-19 09:35:12 -0500

    Fixing link in error message (#1029)
    # What does this PR do?
    Currently the error message says:
    You can start a discussion to request it on
    https://huggingface.co/aws-neuron/optimum-neuron-cacheAlternatively,
    We just needed an extra space in the log line after the link.
    
    
    ## Before submitting
    - [ x] This PR fixes a typo or improves the docs (you can dismiss the
    other checks if that's the case).
    - [ ] Did you make sure to update the documentation with your changes?
    - [ ] Did you write any new necessary tests?

--- optimum/neuron/vllm/model_loader.py
+++ optimum/neuron/vllm/model_loader.py
@@ -139,7 +139,7 @@ def get_optimum_neuron_model(
                 if torch_dtype is not None:
                     error_msg += f", dtype = {torch_dtype}"
                 error_msg += (
-                    f".You can start a discussion to request it on {hub_cache_url}"
+                    f".You can start a discussion to request it on {hub_cache_url} "
                     "Alternatively, you can export your own neuron model "
                     f"as explained in {neuron_export_url}"
                 )
