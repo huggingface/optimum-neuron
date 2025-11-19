4a49e08afd0c2196be4ed7bac11cedbda7f775d6
David Corvoysier <david@huggingface.co>
2025-11-19 16:06:40 +0000

    perf: move accuracy benchmark to vllm

---
+++ benchmark/vllm/performance.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+model=${1:-meta-llama/Meta-Llama-3.1-8B-Instruct}
+users=${2:-128}
+
+date_str=$(date '+%Y-%m-%d-%H-%M-%S')
+output_path="${model//\//_}#${date_str}_guidellm_report.json"
+
+export HF_TOKEN=$(cat ~/.cache/huggingface/token)
+
+export GUIDELLM__NUM_SWEEP_PROFILES=0
+export GUIDELLM__MAX_CONCURRENCY=${users}
+export GUIDELLM__REQUEST_TIMEOUT=60
+
+guidellm \
+  --target "http://localhost:8080/v1" \
+  --model ${model} \
+  --data-type emulated \
+  --data "prompt_tokens=1500,prompt_tokens_variance=150,generated_tokens=250,generated_tokens_variance=20" \
+  --output-path ${output_path} \
