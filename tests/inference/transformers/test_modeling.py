ceada73a481b2b4bfd48e1ea9af78a1de1161b70
JingyaHuang <huang_jingya@outlook.com>
2025-12-02 10:30:20 +0000

    use direct raw link

--- tests/inference/transformers/test_modeling.py
+++ tests/inference/transformers/test_modeling.py
@@ -380,7 +380,7 @@ class NeuronSentenceTransformersIntegrationTest(NeuronModelTestMixin):
 
         texts = ["Two dogs in the snow", "A cat on a table", "A picture of London at night"]
         util.http_get(
-            "https://github.com/UKPLab/sentence-transformers/raw/main/examples/sentence_transformer/applications/image-search/two_dogs_in_snow.jpg",
+            "https://raw.githubusercontent.com/huggingface/sentence-transformers/main/examples/sentence_transformer/applications/image-search/two_dogs_in_snow.jpg",
             "two_dogs_in_snow.jpg",
         )
 
