89b9b1b143dcc6f5ed7dc0f35a1689eaa76e14c1
David Corvoysier <david@huggingface.co>
2026-01-19 15:02:10 +0000

    refactor(ecr): remove tgi image

--- tests/sagemaker/test_images_uri.py
+++ tests/sagemaker/test_images_uri.py
@@ -6,16 +6,13 @@ from optimum.neuron.utils import ecr
 def test_ecr_image_uri():
     region = "eu-west-1"
     account_id = ecr.ACCOUNT_IDS[region]
-    image_uri = ecr.image_uri(region=region, service_name="tgi", version="3.3.4")
-    assert image_uri.startswith(f"{account_id}.dkr.ecr.{region}.amazonaws.com/huggingface-pytorch-tgi-inference:")
+    image_uri = ecr.image_uri(region=region, service_name="vllm", version="0.4.2")
+    assert image_uri.startswith(f"{account_id}.dkr.ecr.{region}.amazonaws.com/huggingface-vllm-inference-neuronx:")
     # without params now
     image_uri = ecr.image_uri()
     assert image_uri is not None
-    # only service name, sagemaker style
-    image_uri = ecr.image_uri(service_name="huggingface-neuronx")
-    assert image_uri is not None
     # only version
-    image_uri = ecr.image_uri(version="3.3.4")
+    image_uri = ecr.image_uri(version="0.4.2")
     assert image_uri is not None
     # invalid version
     image_uri = ecr.image_uri(version="ABCD")
